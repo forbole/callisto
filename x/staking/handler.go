6c06f3db67eb9e3163270a5d7158ffc147442b2e
HarleyAppleChoi <appleno56@gmail.com>
2020-06-19 11:12:52 +0800

    Added handle validator self delegation

---
+++ x/staking/handler.go
@@ -0,0 +1,77 @@
+package staking
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	"github.com/desmos-labs/juno/parse/client"
+	"github.com/desmos-labs/juno/parse/worker"
+	"github.com/desmos-labs/juno/types"
+	"github.com/forbole/bdjuno/database"
+	"github.com/rs/zerolog/log"
+
+	"fmt"
+)
+
+func MsgHandler(tx types.Tx, index int, msg sdk.Msg, w worker.Worker) error {
+	log.Info().Str("tx_hash", tx.TxHash).Int("msg_index", index).Str("msg_type", msg.Type()).Msg("found message")
+
+	if len(tx.Logs) == 0 {
+		log.Info().Msg(fmt.Sprintf("Skipping message at index %d of tx hash %s as it was not successull",
+			index, tx.TxHash))
+		return nil
+	}
+	db, ok := w.Db.(database.BigDipperDb)
+	if !ok {
+		return fmt.Errorf("invalid database")
+	}
+	switch stakingMsg := msg.(type) {
+	case staking.MsgEditValidator:
+		// TODO: Handle message here
+		//store commission rate
+		StoreModifiedCommission(stakingMsg, w.ClientProxy, db)
+	case staking.MsgDelegate:
+		StoreDelegation(stakingMsg,w.ClientProxy,db)
+	}
+
+	return nil
+}
+
+//once there is delegation, store current volting power
+//if delegators delegation address is the self delegation address
+//store the /staking/delegators/{delegatorAddr}/delegations/{validatorAddr} 
+//else store in other table and see what happen
+func StoreDelegation(msg staking.MsgDelegate, cp client.ClientProxy, db database.BigDipperDb) error{
+	validatorAddress := msg.ValidatorAddress
+	deligatorAddress := msg.DelegatorAddress
+	if found, _ := db.HasValidator(validatorAddress.String()); !found {
+		return nil
+	}
+	if found, _ := db.HasValidator(deligatorAddress.String()); !found {
+		return nil
+	}
+	selfAddress := sdk.AccAddress(validatorAddress.Bytes())
+	if (deligatorAddress.Equals(selfAddress)){
+		//DO SOMETHING
+		StoreSelfDelegation(validatorAddress)
+	}
+
+}
+
+func StoreModifiedCommission(msg staking.MsgEditValidator, cp client.ClientProxy, db database.BigDipperDb) error {
+	//should I take from REST or store the message?
+	//store the message
+	address := msg.ValidatorAddress
+	if found, _ := db.HasValidator(address.String()); !found {
+		return nil
+	}
+
+	var validator staking.Validator
+	endpoint := fmt.Sprintf("/staking/validators/$s", address.String())
+	height, ok := cp.QueryLCDWithHeight(endpoint, &validator)
+	if ok != nil {
+		return ok
+	}
+
+	db.SaveVaildatorComission(validator, height)
+	return nil
+}
