100d619827cee39b93197b83aab3db6e542da411
HarleyAppleChoi <appleno56@gmail.com>
2020-06-22 11:18:11 +0800

    Added essentials for handler

--- x/staking/handler.go
+++ x/staking/handler.go
@@ -15,17 +15,30 @@ import (
 )
 
 func MsgHandler(tx types.Tx, index int, msg sdk.Msg, w worker.Worker) error {
-	log.Info().Str("tx_hash", tx.TxHash).Int("msg_index", index).Str("msg_type", msg.Type()).Msg("found message")
+	log.Info().Str("tx_hash", tx.TxHash).Int("msg_index", index).
+	Str("msg_type", msg.Type()).Msg("found message")
 
-	if len(tx.Logs) == 0 {
-		log.Info().Msg(fmt.Sprintf("Skipping message at index %d of tx hash %s as it was not successull",
-			index, tx.TxHash))
-		return nil
-	}
-	db, ok := w.Db.(database.BigDipperDb)
-	if !ok {
-		return fmt.Errorf("invalid database")
-	}
+if len(tx.Logs) == 0 {
+	log.Info().Str("tx_hash", tx.TxHash).Int("msg_index", index).
+		Msg("skipping message as it was not successful")
+	return nil
+}
+
+db, ok := w.Db.(database.BigDipperDb)
+if !ok {
+	return fmt.Errorf("given database instance is not a BigDipperDb")
+}
+
+timestamp, err := time.Parse("2006-01-02T15:04:05Z", tx.Timestamp)
+if err != nil {
+	return err
+}
+if len(tx.Logs) == 0 {
+	log.Info().Msg(fmt.Sprintf("Skipping message at index %d of tx hash %s as it was not successull",
+		index, tx.TxHash))
+	return nil
+}
+
 	switch stakingMsg := msg.(type) {
 	case staking.MsgEditValidator:
 		// TODO: Handle message here
