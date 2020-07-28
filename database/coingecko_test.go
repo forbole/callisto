79d5082f8b5fad4b282c4afdaf12a080d4f2b08b
HYChoi <appleno56@gmail.com>
2020-07-28 13:47:18 +0800

    Add test cases

--- database/coingecko_test.go
+++ database/coingecko_test.go
@@ -1,27 +1,41 @@
 package database_test
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	dbtypes "github.com/forbole/bdjuno/database/types"
+	api "github.com/forbole/bdjuno/x/coingecko/apiTypes"
 )
 
-func (suite *DbTestSuite) TestBigDipperDb_SaveTotalTokens() {
-	coins := sdk.NewCoins(
-		sdk.NewCoin("desmos", sdk.NewInt(10000)),
-		sdk.NewCoin("uatom", sdk.NewInt(15)),
-	)
-	err := suite.database.SaveSupplyToken(coins, 10)
+func (suite *DbTestSuite) TestBigDipperDb_SaveTokenPrice() {
+	market := []api.Market{
+		api.NewMarket(
+			"udaric",
+			100.01,
+			10,
+		),
+		api.NewMarket(
+			"utopi",
+			200.01,
+			20,
+		),
+	}
+	err := suite.database.SaveTokensPrice(market, 30)
 	suite.Require().NoError(err)
 
-	expected := dbtypes.NewTotalSupplyRow(
-		dbtypes.NewDbCoins(coins),
-		10,
-	)
-	var rows []dbtypes.TotalSupplyRow
-	err = suite.database.Sqlx.Select(&rows, `SELECT coins,height FROM supply`)
+	expected := []dbtypes.TokenPriceRow{
+		dbtypes.NewTokenPriceRow("udaric",
+			100.01,
+			10,
+			30),
+		dbtypes.NewTokenPriceRow("utopi",
+			200.01,
+			20,
+			30,
+		),
+	}
+	var rows []dbtypes.TokenPriceRow
+	err = suite.database.Sqlx.Select(&rows, `SELECT denom,current_price,market_cap,height FROM token_values`)
 	suite.Require().NoError(err)
-	suite.Require().Len(rows, 1, "supply table should contain only one row")
-
-	suite.Require().True(expected.Equals(rows[0]))
-
+	for i,row:=range(rows){
+	suite.Require().True(expected[i].Equals(row))
+	}
 }
---
+++ database/types/coingecko.go
@@ -0,0 +1,24 @@
+package types
+
+type TokenPriceRow struct {
+	Denom         string
+	Current_price float64
+	Market_cap    int64
+	Height        int64
+}
+
+func NewTokenPriceRow(denom string, current_price float64, market_cap int64, height int64) TokenPriceRow {
+	return TokenPriceRow{
+		Denom:         denom,
+		Current_price: current_price,
+		Market_cap:    market_cap,
+		Height:        height,
+	}
+}
+
+func (u TokenPriceRow) Equals(v TokenPriceRow) bool {
+	return u.Denom == v.Denom &&
+		u.Current_price == v.Current_price &&
+		u.Market_cap == v.Market_cap &&
+		u.Height == v.Height
+}
