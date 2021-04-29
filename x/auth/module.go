02713ea4807d9e24617bf67e0db6c9aed8bcb4f2
riccardo.montagnin <riccardo.montagnin@gmail.com>
2021-04-29 15:49:56 +0200

    Started implementing the fast sync

--- x/auth/module.go
+++ x/auth/module.go
@@ -17,9 +17,10 @@ import (
 )
 
 var (
-	_ modules.Module        = &Module{}
-	_ modules.GenesisModule = &Module{}
-	_ modules.MessageModule = &Module{}
+	_ modules.Module         = &Module{}
+	_ modules.GenesisModule  = &Module{}
+	_ modules.MessageModule  = &Module{}
+	_ modules.FastSyncModule = &Module{}
 )
 
 // Module represents the x/auth module
@@ -46,6 +47,11 @@ func NewModule(
 // Name implements modules.Module
 func (m *Module) Name() string {
 	return "auth"
+}
+
+// DownloadState implements modules.FastSyncModule
+func (m *Module) DownloadState(height int64) error {
+	return FastSync(height, m.authClient, m.db)
 }
 
 // HandleGenesis implements modules.GenesisModule
