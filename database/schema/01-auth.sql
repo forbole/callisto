92f021577b68f5bbe465ff1075e70a0365a35113
HYChoi <appleno56@gmail.com>
2021-07-08 18:22:30 +0100

    Get Vesting base on vestingAccountYAML in cosmos

--- database/schema/01-auth.sql
+++ database/schema/01-auth.sql
@@ -2,3 +2,25 @@ CREATE TABLE account
 (
     address TEXT NOT NULL PRIMARY KEY
 );
+
+CREATE TYPE PERIOD AS
+(
+    length  BIGINT,
+    amount COIN[]
+);
+
+# vestingAccountYAML
+CREATE TABLE vestingAccount
+(
+    address TEXT NOT NULL REFERENCES account (address) PRIMARY KEY,
+    pub_key TEXT NOT NULL,
+    account_number TEXT NOT NULL,
+    sequence TEXT NOT NULL,
+    original_vesting COIN[],
+    delegated_free   COIN[],
+    delegated_vesting COIN[],
+    end_time BIGINT,
+    StartTime BIGINT,
+    VestingPeriods PERIOD[]
+);
+
