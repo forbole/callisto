1b7ba77bf8d9664d412e43b80da29ebe0409c704
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-08-20 10:37:03 +0200

    Removed some useless block.height references

--- database/schema/03-staking.sql
+++ database/schema/03-staking.sql
@@ -156,7 +156,7 @@ CREATE TABLE unbonding_delegation
     delegator_address    TEXT                        NOT NULL REFERENCES account (address),
     amount               COIN                        NOT NUll,
     completion_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    height               BIGINT                      NOT NULL REFERENCES block (height),
+    height               BIGINT                      NOT NULL,
     CONSTRAINT unbonding_delegation_validator_delegator_unique UNIQUE (delegator_address, validator_address,
                                                                        amount, completion_timestamp)
 );
