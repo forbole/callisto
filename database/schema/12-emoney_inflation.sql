8f869e3c0a839b36f224febca32c955e85aea0fd
huichiaotsou <huichiao.tsou@gmail.com>
2021-09-23 13:49:43 +0800

    modify schema 12: emoney inflation

--- database/schema/12-emoney_inflation.sql
+++ database/schema/12-emoney_inflation.sql
@@ -2,9 +2,10 @@
 
 CREATE TABLE emoney_inflation
 (
-    one_row_id              bool        PRIMARY KEY DEFAULT TRUE,
+    one_row_id              BOOLEAN     NOT NULL DEFAULT TRUE PRIMARY KEY,
     inflation               JSONB       NOT NULL,
     last_applied_time       TIMESTAMP   NOT NULL,
-    last_applied_height     INT         NOT NULL,
+    last_applied_height     BIGINT      NOT NULL,
+    height                  BIGINT      NOT NULL,
     CHECK (one_row_id)
 );
