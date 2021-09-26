67b680dbd05f2ba51d4353b267431a1dd3d3ec1f
huichiaotsou <huichiao.tsou@gmail.com>
2021-09-26 12:11:57 +0800

    add DB types: EMoneyGasPricesRow

--- database/types/emoney.go
+++ database/types/emoney.go
@@ -2,6 +2,8 @@ package types
 
 import "time"
 
+// ---------------------------- EMoneyInflationRow ----------------------------
+
 // EMoneyInflationRow represents a single row of the emoney_inflation table
 type EMoneyInflationRow struct {
 	OneRowId          bool      `db:"one_row_id"`
@@ -33,5 +35,34 @@ func (v EMoneyInflationRow) Equal(w EMoneyInflationRow) bool {
 	return v.Inflation == w.Inflation &&
 		v.LastAppliedTime == w.LastAppliedTime &&
 		v.LastAppliedHeight == w.LastAppliedHeight &&
+		v.Height == w.Height
+}
+
+// ---------------------------- EMoneyGasPrices ----------------------------
+
+// EMoneyGasPricesRow represents a single row of the emoney_gas_prices table
+type EMoneyGasPricesRow struct {
+	AuthorityKey string `db:"authority_key"`
+	GasPrices    string `db:"gas_prices"`
+	Height       int64  `db:"height"`
+}
+
+// EMoneyGasPricesRow allows to build a new EmoneyGasPricesRow
+func NewEMoneyGasPricesRow(
+	authority_key string,
+	gas_prices string,
+	height int64,
+) EMoneyGasPricesRow {
+	return EMoneyGasPricesRow{
+		AuthorityKey: authority_key,
+		GasPrices:    gas_prices,
+		Height:       height,
+	}
+}
+
+// Equal tells whether v and w represent the same rows
+func (v EMoneyGasPricesRow) Equal(w EMoneyGasPricesRow) bool {
+	return v.AuthorityKey == w.AuthorityKey &&
+		v.GasPrices == w.GasPrices &&
 		v.Height == w.Height
 }
