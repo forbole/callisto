30f23c561001b6f528ec0995814d0bc06629557e
huichiaotsou <huichiao.tsou@gmail.com>
2021-10-13 19:00:15 +0800

    add schema vesting_account

--- database/schema/01-auth.sql
+++ database/schema/01-auth.sql
@@ -2,3 +2,18 @@ CREATE TABLE account
 (
     address TEXT NOT NULL PRIMARY KEY
 );
+
+CREATE TYPE VESTING_PERIOD
+(
+    length      TEXT    NOT NULL,
+    amounts     COIN[]  NOT NULL DEFAULT '{}'
+);
+
+CREATE TABLE vesting_account
+(
+    address             TEXT                NOT NULL REFERENCES account (address) PRIMARY KEY,
+    original_vesting    COIN[]              NOT NULL DEFAULT '{}',
+    end_time            TIMESTAMP           NOT NULL,
+    start_time          TIMESTAMP           NOT NULL,
+    vesting_periods     VESTING_PERIOD[]    NOT NULL DEFAULT '{}'
+);
