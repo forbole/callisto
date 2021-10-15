d60069081d979184adf0f3090933cc1dc457cdde
huichiaotsou <huichiao.tsou@gmail.com>
2021-10-15 10:22:55 +0800

    add structure in auth/handle_genesis.go

--- modules/auth/handle_genesis.go
+++ modules/auth/handle_genesis.go
@@ -13,6 +13,7 @@ import (
 func (m *Module) HandleGenesis(_ *tmtypes.GenesisDoc, appState map[string]json.RawMessage) error {
 	log.Debug().Str("module", "auth").Msg("parsing genesis")
 
+	// Handle Genesis account addresses
 	accounts, err := GetGenesisAccounts(appState, m.cdc)
 	if err != nil {
 		return fmt.Errorf("error while getting genesis accounts: %s", err)
@@ -21,6 +22,18 @@ func (m *Module) HandleGenesis(_ *tmtypes.GenesisDoc, appState map[string]json.R
 	err = m.db.SaveAccounts(accounts)
 	if err != nil {
 		return fmt.Errorf("error while storing genesis accounts: %s", err)
+	}
+
+	// Handle Genesis vesting account details
+	vestingAccounts, err := GetGenesisVestingAccounts(appState)
+	if err != nil {
+		return fmt.Errorf("error while getting genesis vesting accounts: %s", err)
+	}
+
+	paramsNumber := 5
+	err = m.db.SaveVestingAccounts(paramsNumber, vestingAccounts)
+	if err != nil {
+		return fmt.Errorf("error while storing genesis vesting accounts: %s", err)
 	}
 
 	return nil
