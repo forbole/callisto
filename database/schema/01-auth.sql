c1a358831086ef488a7931a0d628bb6ef397aa2e
huichiaotsou <huichiao.tsou@gmail.com>
2021-10-15 10:14:04 +0800

    schema

--- database/schema/01-auth.sql
+++ database/schema/01-auth.sql
@@ -3,17 +3,12 @@ CREATE TABLE account
     address TEXT NOT NULL PRIMARY KEY
 );
 
-CREATE TYPE VESTING_PERIOD
-(
-    length      TEXT    NOT NULL,
-    amounts     COIN[]  NOT NULL DEFAULT '{}'
-);
 
 CREATE TABLE vesting_account
 (
-    address             TEXT                NOT NULL REFERENCES account (address) PRIMARY KEY,
-    original_vesting    COIN[]              NOT NULL DEFAULT '{}',
-    end_time            TIMESTAMP           NOT NULL,
-    start_time          TIMESTAMP           NOT NULL,
-    vesting_periods     VESTING_PERIOD[]    NOT NULL DEFAULT '{}'
+    address             TEXT        NOT NULL REFERENCES account (address) PRIMARY KEY,
+    original_vesting    JSONB       NOT NULL DEFAULT '{}',
+    end_time            TIMESTAMP   NOT NULL,
+    start_time          TIMESTAMP   NOT NULL,
+    vesting_periods     JSONB       NOT NULL DEFAULT '{}'
 );
