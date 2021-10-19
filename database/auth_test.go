c0061950024aa5ec308a8f28248562f836efc7f1
huichiaotsou <huichiao.tsou@gmail.com>
2021-10-19 16:37:05 +0800

    add unit test

--- database/auth_test.go
+++ database/auth_test.go
@@ -1,8 +1,12 @@
 package database_test
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authttypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/vesting/exported"
+	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 
 	"github.com/forbole/bdjuno/v2/types"
 
@@ -70,4 +74,131 @@ func (suite *DbTestSuite) TestBigDipperDb_GetAccounts() {
 	for index, acc := range expectedAccs {
 		suite.Require().Equal(acc, accounts[index])
 	}
+}
+
+func (suite *DbTestSuite) Test_SaveVestingAccounts() {
+	// --- Save account addresses for foreign key contraint ---
+	queries := []string{
+		`INSERT INTO account (address) VALUES ('cosmos1ltzt0z992ke6qgmtjxtygwzn36km4cy6cqdknt')`,
+		`INSERT INTO account (address) VALUES ('cosmos1re6zjpyczs0w7flrl6uacl0r4teqtyg62crjsn')`,
+		`INSERT INTO account (address) VALUES ('cosmos1eg47ue0l85lzkfgc4leske6hcah8cz3qajpjy2')`,
+	}
+	for _, query := range queries {
+		_, err := suite.database.Sql.Exec(query)
+		suite.Require().NoError(err)
+	}
+	var accountRows []dbtypes.AccountRow
+	suite.database.Sqlx.Select(&accountRows, `SELECT * FROM account`)
+
+	// --- Prepare VestingAccounts for saving into DB ---
+	sdkCoins := sdk.NewCoins(sdk.NewCoin("desmos", sdk.NewInt(10)))
+	// ContinuousVestingAccount
+	address1, err := sdk.AccAddressFromBech32("cosmos1ltzt0z992ke6qgmtjxtygwzn36km4cy6cqdknt")
+	suite.Require().NoError(err)
+	ContinuousVestingAccount := vestingtypes.NewContinuousVestingAccount(
+		authttypes.NewBaseAccountWithAddress(address1),
+		sdkCoins,
+		time.Date(1990, 9, 9, 00, 00, 00, 000, time.UTC).Unix(), // Start Time
+		time.Date(2020, 9, 9, 00, 00, 00, 000, time.UTC).Unix(), // End Time
+	)
+
+	// DelayedVestingAccount
+	address2, _ := sdk.AccAddressFromBech32("cosmos1re6zjpyczs0w7flrl6uacl0r4teqtyg62crjsn")
+	DelayedVestingAccount := vestingtypes.NewDelayedVestingAccount(
+		authttypes.NewBaseAccountWithAddress(address2),
+		sdkCoins,
+		time.Date(2020, 9, 9, 00, 00, 00, 000, time.UTC).Unix(), // End Time
+	)
+
+	// PeriodicVestingAccount
+	address3, _ := sdk.AccAddressFromBech32("cosmos1eg47ue0l85lzkfgc4leske6hcah8cz3qajpjy2")
+	periods := []vestingtypes.Period{
+		{
+			Length: 2629743,
+			Amount: sdkCoins,
+		},
+		{
+			Length: 7889229,
+			Amount: sdkCoins,
+		},
+	}
+	PeriodicVestingAccount := vestingtypes.NewPeriodicVestingAccount(
+		authttypes.NewBaseAccountWithAddress(address3),
+		sdkCoins,
+		time.Date(1990, 9, 9, 00, 00, 00, 000, time.UTC).Unix(), // Start Time
+		periods,
+	)
+
+	// VestingAccounts
+	vestingAccounts := []exported.VestingAccount{
+		ContinuousVestingAccount,
+		DelayedVestingAccount,
+		PeriodicVestingAccount,
+	}
+
+	// --- Save the data into DB ---
+	err = suite.database.SaveVestingAccounts(vestingAccounts)
+	suite.Require().NoError(err)
+
+	// --- Verify Continuous Vesting Account ---
+	var continuousVestingAccountRow []dbtypes.ContinuousVestingAccountRow
+	err = suite.database.Sqlx.Select(&continuousVestingAccountRow, `SELECT * FROM vesting_account WHERE type = 'ContinuousVestingAccount'`)
+	suite.Require().NoError(err)
+	suite.Require().Len(continuousVestingAccountRow, 1, "ContinuousVestingAccount type should contain only one row")
+
+	expectedContinuousVestingAccountRow := dbtypes.NewContinuousVestingAccountRow(
+		1,
+		"ContinuousVestingAccount",
+		"cosmos1ltzt0z992ke6qgmtjxtygwzn36km4cy6cqdknt",
+		dbtypes.NewDbCoins(sdkCoins),
+		time.Date(2020, 9, 9, 00, 00, 00, 000, time.UTC), // EndTime
+		time.Date(1990, 9, 9, 00, 00, 00, 000, time.UTC), // StartTime
+	)
+	suite.Require().True(expectedContinuousVestingAccountRow.Equal(continuousVestingAccountRow[0]))
+
+	// --- Verify Delayed Vesting Account ---
+	var delayedVestingAccountRow []dbtypes.DelayedVestingAccountRow
+	err = suite.database.Sqlx.Select(&delayedVestingAccountRow, `SELECT id, type, address, original_vesting, end_time FROM vesting_account WHERE type = 'DelayedVestingAccount'`)
+	suite.Require().NoError(err)
+	suite.Require().Len(delayedVestingAccountRow, 1, "DelayedVestingAccountRow type should contain only one row")
+
+	expectedDelayedVestingAccountRow := dbtypes.NewDelayedVestingAccountRow(
+		2,
+		"DelayedVestingAccount",
+		"cosmos1re6zjpyczs0w7flrl6uacl0r4teqtyg62crjsn",
+		dbtypes.NewDbCoins(sdkCoins),
+		time.Date(2020, 9, 9, 00, 00, 00, 000, time.UTC), // EndTime
+	)
+	suite.Require().True(expectedDelayedVestingAccountRow.Equal(delayedVestingAccountRow[0]))
+
+	// --- Verify Periodic Vesting Account ---
+	var periodicVestingAccountRow []dbtypes.PeriodicVestingAccountRow
+	err = suite.database.Sqlx.Select(&periodicVestingAccountRow, `SELECT * FROM vesting_account WHERE type = 'PeriodicVestingAccount'`)
+	suite.Require().NoError(err)
+	suite.Require().Len(delayedVestingAccountRow, 1, "DelayedVestingAccountRow type should contain only one row")
+
+	expectedPeriodicVestingAccountRow := dbtypes.NewPeriodicVestingAccountRow(
+		3,
+		"PeriodicVestingAccount",
+		"cosmos1eg47ue0l85lzkfgc4leske6hcah8cz3qajpjy2",
+		dbtypes.NewDbCoins(sdkCoins),
+		time.Date(2020, 9, 9, 00, 00, 00, 000, time.UTC), // EndTime
+		time.Date(1990, 9, 9, 00, 00, 00, 000, time.UTC), // StartTime
+	)
+	suite.Require().True(expectedPeriodicVestingAccountRow.Equal(periodicVestingAccountRow[0]))
+
+	// --- Verify vesting periods ---
+	var vestingPeriodRows []dbtypes.VestingPeriodRow
+	err = suite.database.Sqlx.Select(&vestingPeriodRows, `SELECT * FROM vesting_period`)
+	suite.Require().NoError(err)
+	suite.Require().Len(vestingPeriodRows, 2, "vestingPeriodRows should contain only 2 rows")
+
+	expectedVestingPeriods := []dbtypes.VestingPeriodRow{
+		dbtypes.NewVestingPeriodRow(3, 0, "2629743", dbtypes.NewDbCoins(sdkCoins)),
+		dbtypes.NewVestingPeriodRow(3, 1, "7889229", dbtypes.NewDbCoins(sdkCoins)),
+	}
+	for index, vestingPeriod := range expectedVestingPeriods {
+		suite.Require().True(vestingPeriod.Equal(vestingPeriodRows[index]))
+	}
+
 }
--- database/types/auth.go
+++ database/types/auth.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"fmt"
+	"time"
+)
+
 // AccountRow represents a single row inside the account table
 type AccountRow struct {
 	Address string `db:"address"`
@@ -15,4 +20,146 @@ func NewAccountRow(address string) AccountRow {
 // Equal tells whether a and b contain the same data
 func (a AccountRow) Equal(b AccountRow) bool {
 	return a.Address == b.Address
+}
+
+// --------------- For Vesting Accounts ---------------
+
+// ContinuousVestingAccountRow represents a single row inside the vesting_account table
+type ContinuousVestingAccountRow struct {
+	Id              int       `db:"id"`
+	Type            string    `db:"type"`
+	Address         string    `db:"address"`
+	OriginalVesting *DbCoins  `db:"original_vesting"`
+	EndTime         time.Time `db:"end_time"`
+	StartTime       time.Time `db:"start_time"`
+}
+
+// NewContinuousVestingAccountRow allows to build a new DB ContinuousVestingAccountRow
+func NewContinuousVestingAccountRow(
+	id int,
+	accountType string,
+	address string,
+	originalVesting DbCoins,
+	endTime time.Time,
+	startTime time.Time,
+) ContinuousVestingAccountRow {
+	return ContinuousVestingAccountRow{
+		Id:              id,
+		Type:            accountType,
+		Address:         address,
+		OriginalVesting: &originalVesting,
+		EndTime:         endTime,
+		StartTime:       startTime,
+	}
+}
+
+// Equal tells whether a and b contain the same data
+func (a ContinuousVestingAccountRow) Equal(b ContinuousVestingAccountRow) bool {
+	return a.Id == b.Id &&
+		a.Type == b.Type &&
+		a.Address == b.Address &&
+		a.OriginalVesting.Equal(b.OriginalVesting)
+}
+
+// DelayedVestingAccountRow represents a single row inside the vesting_account table
+type DelayedVestingAccountRow struct {
+	Id              int       `db:"id"`
+	Type            string    `db:"type"`
+	Address         string    `db:"address"`
+	OriginalVesting *DbCoins  `db:"original_vesting"`
+	EndTime         time.Time `db:"end_time"`
+}
+
+// NewDelayedVestingAccountRow allows to build a new DB DelayedVestingAccountRow
+func NewDelayedVestingAccountRow(
+	id int,
+	accountType string,
+	address string,
+	originalVesting DbCoins,
+	endTime time.Time,
+) DelayedVestingAccountRow {
+	return DelayedVestingAccountRow{
+		Id:              id,
+		Type:            accountType,
+		Address:         address,
+		OriginalVesting: &originalVesting,
+		EndTime:         endTime,
+	}
+}
+
+// Equal tells whether a and b contain the same data
+func (a DelayedVestingAccountRow) Equal(b DelayedVestingAccountRow) bool {
+	return a.Id == b.Id &&
+		a.Type == b.Type &&
+		a.Address == b.Address &&
+		a.OriginalVesting.Equal(b.OriginalVesting)
+}
+
+// PeriodicVestingAccountRow represents a single row inside the vesting_account table
+type PeriodicVestingAccountRow struct {
+	Id              int       `db:"id"`
+	Type            string    `db:"type"`
+	Address         string    `db:"address"`
+	OriginalVesting *DbCoins  `db:"original_vesting"`
+	EndTime         time.Time `db:"end_time"`
+	StartTime       time.Time `db:"start_time"`
+}
+
+// NewPeriodicVestingAccountRow allows to build a new DB PeriodicVestingAccountRow
+func NewPeriodicVestingAccountRow(
+	id int,
+	accountType string,
+	address string,
+	originalVesting DbCoins,
+	endTime time.Time,
+	startTime time.Time,
+) PeriodicVestingAccountRow {
+	return PeriodicVestingAccountRow{
+		Id:              id,
+		Type:            accountType,
+		Address:         address,
+		OriginalVesting: &originalVesting,
+		EndTime:         endTime,
+		StartTime:       startTime,
+	}
+}
+
+// Equal tells whether a and b contain the same data
+func (a PeriodicVestingAccountRow) Equal(b PeriodicVestingAccountRow) bool {
+	return a.Id == b.Id &&
+		a.Type == b.Type &&
+		a.Address == b.Address &&
+		a.OriginalVesting.Equal(b.OriginalVesting)
+}
+
+// VestingPeriodRow represents a Periodic Vesting Account
+type VestingPeriodRow struct {
+	VestingAccountID int      `db:"vesting_account_id"`
+	PeriodOrder      int      `db:"period_order"`
+	Length           string   `db:"length"`
+	Amount           *DbCoins `db:"amount"`
+}
+
+// NewPeriodicVestingAccountRow allows to build a new DB PeriodicVestingAccountRow
+func NewVestingPeriodRow(
+	vestingAccountID int,
+	periodOrder int,
+	length string,
+	amount DbCoins,
+) VestingPeriodRow {
+	return VestingPeriodRow{
+		VestingAccountID: vestingAccountID,
+		PeriodOrder:      periodOrder,
+		Length:           length,
+		Amount:           &amount,
+	}
+}
+
+// Equal tells whether a and b contain the same data
+func (a VestingPeriodRow) Equal(b VestingPeriodRow) bool {
+	fmt.Println(a, b)
+	return a.VestingAccountID == b.VestingAccountID &&
+		a.PeriodOrder == b.PeriodOrder &&
+		a.Length == b.Length &&
+		a.Amount.Equal(b.Amount)
 }
