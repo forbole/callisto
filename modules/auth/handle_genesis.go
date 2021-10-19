e0e4f64a494afefe4cb2ea56d38ca903f6b4c892
huichiaotsou <huichiao.tsou@gmail.com>
2021-10-19 10:55:37 +0800

    implement GetGenesisVestingAccounts and use in HandleGenesis

--- modules/auth/handle_genesis.go
+++ modules/auth/handle_genesis.go
@@ -11,16 +11,27 @@ import (
 
 // HandleGenesis implements modules.GenesisModule
 func (m *Module) HandleGenesis(_ *tmtypes.GenesisDoc, appState map[string]json.RawMessage) error {
-	log.Debug().Str("module", "auth").Msg("parsing genesis")
 
+	// Handle account addresses
+	log.Debug().Str("module", "auth").Msg("parsing genesis")
 	accounts, err := GetGenesisAccounts(appState, m.cdc)
 	if err != nil {
 		return fmt.Errorf("error while getting genesis accounts: %s", err)
 	}
-
 	err = m.db.SaveAccounts(accounts)
 	if err != nil {
 		return fmt.Errorf("error while storing genesis accounts: %s", err)
+	}
+
+	// Handle storing vesting accounts
+	log.Debug().Str("module", "auth/vesting").Msg("parsing genesis")
+	vestingAccounts, err := GetGenesisVestingAccounts(appState, m.cdc)
+	if err != nil {
+		return fmt.Errorf("error while getting genesis vesting accounts: %s", err)
+	}
+	err = m.db.SaveVestingAccounts(vestingAccounts)
+	if err != nil {
+		return fmt.Errorf("error while storing genesis vesting accounts: %s", err)
 	}
 
 	return nil
