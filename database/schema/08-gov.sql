482aa4fcea4c079a342a70424ea54abf0c399257
Magic Cat <pusz.monika@gmail.com>
2021-11-11 16:10:47 +0000

    Update proposal_tally_result

--- database/schema/08-gov.sql
+++ database/schema/08-gov.sql
@@ -52,10 +52,10 @@ CREATE INDEX proposal_vote_height_index ON proposal_vote (height);
 CREATE TABLE proposal_tally_result
 (
     proposal_id  INTEGER REFERENCES proposal (id) PRIMARY KEY,
-    yes          BIGINT NOT NULL,
-    abstain      BIGINT NOT NULL,
-    no           BIGINT NOT NULL,
-    no_with_veto BIGINT NOT NULL,
+    yes          TEXT NOT NULL,
+    abstain      TEXT NOT NULL,
+    no           TEXT NOT NULL,
+    no_with_veto TEXT NOT NULL,
     height       BIGINT NOT NULL,
     CONSTRAINT unique_tally_result UNIQUE (proposal_id)
 );
