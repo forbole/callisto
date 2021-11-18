f752e10d580f88117e46d20c7c9c6b3d354f4955
huichiaotsou <huichiao.tsou@gmail.com>
2021-11-18 16:51:14 +0800

    add schema

---
+++ database/schema/12-profiles.sql
@@ -0,0 +1,11 @@
+CREATE TABLE profiles_params
+(
+    one_row_id          BOOLEAN NOT NULL DEFAULT TRUE PRIMARY KEY,
+    nickname_params     JSONB   NOT NULL,
+    d_tag_params        JSONB   NOT NULL,
+    bio_params          JSONB   NOT NULL,
+    oracle_params       JSONB   NOT NULL,
+    height              BIGINT  NOT NULL,
+    CHECK (one_row_id)
+);
+CREATE INDEX profiles_params_height_index ON profiles_params (height);
