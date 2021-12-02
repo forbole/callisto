10cdda693f5ef0dfabb8da344940cf98ec221fd6
Magic Cat <pusz.monika@gmail.com>
2021-12-02 13:47:47 +0000

    Update comment

--- cmd/fix/feegrant/cmd.go
+++ cmd/fix/feegrant/cmd.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// NewStakingCmd returns the Cobra command that allows to fix all the things related to the x/staking module
+// NewFeegrantCmd returns the Cobra command that allows to fix all the things related to the x/feegrant module
 func NewFeegrantCmd(parseConfig *parse.Config) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "feegrant",
