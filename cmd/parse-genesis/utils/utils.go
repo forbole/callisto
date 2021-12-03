ace61192b876ef9c6bec7785c314e1f98599c56e
huichiaotsou <huichiao.tsou@gmail.com>
2021-12-03 18:38:23 +0800

    remove method getRegisteredModules

--- cmd/parse-genesis/utils/utils.go
+++ cmd/parse-genesis/utils/utils.go
@@ -5,42 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	parsecmd "github.com/forbole/juno/v2/cmd/parse"
-	"github.com/forbole/juno/v2/database"
 	"github.com/forbole/juno/v2/modules"
-	modsregistrar "github.com/forbole/juno/v2/modules/registrar"
 	"github.com/forbole/juno/v2/types/config"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
-
-// GetRegisteredModules returns the registered modules from the config file
-func GetRegisteredModules(parseConfig *parsecmd.Config) ([]modules.Module, error) {
-	// Get the global config
-	cfg := config.Cfg
-
-	// Build the codec
-	encodingConfig := parseConfig.GetEncodingConfigBuilder()()
-
-	// Setup the SDK configuration
-	sdkConfig := sdk.GetConfig()
-	parseConfig.GetSetupConfig()(cfg, sdkConfig)
-	sdkConfig.Seal()
-
-	// Get the db
-	databaseCtx := database.NewContext(cfg.Database, &encodingConfig, parseConfig.GetLogger())
-	db, err := parseConfig.GetDBBuilder()(databaseCtx)
-	if err != nil {
-		return nil, err
-	}
-
-	// Get the modules
-	context := modsregistrar.NewContext(cfg, sdkConfig, &encodingConfig, db, nil, parseConfig.GetLogger())
-	mods := parseConfig.GetRegistrar().BuildModules(context)
-	registeredModules := modsregistrar.GetModules(mods, cfg.Chain.Modules, parseConfig.GetLogger())
-
-	return registeredModules, nil
-}
 
 // GetGenesisDocAndState generates and returns the genesis doc and genesis state with genesis.json file
 func GetGenesisDocAndState() (*tmtypes.GenesisDoc, map[string]json.RawMessage, error) {
