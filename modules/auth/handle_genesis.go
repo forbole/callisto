7be3cda25a2b3327b8e9ed3c71506ce93ea9f83e
huichiaotsou <huichiao.tsou@gmail.com>
2021-12-07 17:28:50 +0800

    move import position

--- modules/auth/handle_genesis.go
+++ modules/auth/handle_genesis.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/rs/zerolog/log"
 	tmtypes "github.com/tendermint/tendermint/types"
+
+	"github.com/rs/zerolog/log"
 )
 
 // HandleGenesis implements modules.GenesisModule
