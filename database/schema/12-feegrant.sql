43b577d14af25405c6d5ddf41c748a62f70a86a9
Magic Cat <pusz.monika@gmail.com>
2021-12-07 16:29:22 +0000

    Update feegrant.sql

--- database/schema/12-feegrant.sql
+++ database/schema/12-feegrant.sql
@@ -3,7 +3,7 @@ CREATE TABLE fee_grant_allowance
     id                 SERIAL      NOT NULL PRIMARY KEY,
     grantee_address    TEXT        NOT NULL REFERENCES account (address),
     granter_address    TEXT        NOT NULL REFERENCES account (address),
-    allowance          JSONB       NOT NULL,
+    allowance          JSONB       NOT NULL DEFAULT '{}'::JSONB,
     height             BIGINT      NOT NULL,
     CONSTRAINT unique_fee_grant_allowance UNIQUE(grantee_address, granter_address) 
 );
