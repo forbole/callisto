861e695357eb77ccbc973f8eb6f0105c81f43179
Magic Cat <pusz.monika@gmail.com>
2022-01-19 16:12:30 +0000

    Clean staking source

--- modules/staking/source/source.go
+++ modules/staking/source/source.go
@@ -7,9 +7,6 @@ import (
 type Source interface {
 	GetValidator(height int64, valOper string) (stakingtypes.Validator, error)
 	GetValidatorsWithStatus(height int64, status string) ([]stakingtypes.Validator, error)
-	GetDelegation(height int64, delegator string, validator string) (stakingtypes.DelegationResponse, error)
-	GetDelegatorDelegations(height int64, delegator string) ([]stakingtypes.DelegationResponse, error)
-	GetValidatorDelegations(height int64, validator string) ([]stakingtypes.DelegationResponse, error)
 	GetPool(height int64) (stakingtypes.Pool, error)
 	GetParams(height int64) (stakingtypes.Params, error)
 }
