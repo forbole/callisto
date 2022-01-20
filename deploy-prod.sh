d22793b8a15b7bc7cf72ff58b46728d9103c465e
infra1515 <lyubomir.yonchev@limechain.tech>
2022-01-20 14:42:22 +0200

    deploy script added for cudos-bdjuni, docker file altered - one for prod and one for local env ( to do )

---
+++ deploy-prod.sh
@@ -0,0 +1,27 @@
+env=$1 #prod or dev
+
+git clone https://github.com/CudoVentures/cudos-bdjuno.git CudosBDJuno
+git clone https://github.com/CudoVentures/big-dipper-2.0-cosmos.git BigDipper2
+cp -R /bdjuno-config CudosBDJuno/
+cp .env.bdjuno CudosBDJuno/.env
+cp .env.big-dipper-2 BigDipper2/.env
+
+cd CudosBDJuno
+echo "Starting BDJuno docker-compose"
+if [ $env = "prod" ]; then
+    docker-compose up -d --file=docker-compose-prod.yml
+elif [ $env = "dev" ]; then
+    docker-compose up -d --file=docker-compose-dev.yml
+else
+    echo "Wrong env passed: can be either dev or prod"
+fi
+
+# cd ../BigDipper2
+# echo "Starting BigDipper2 docker-compose"
+# if [ $env = "prod" ]; then
+#     docker-compose up -d --file=docker-compose-prod.yml
+# elif [ $env = "dev" ]; then
+#     docker-compose up -d --file=docker-compose-dev.yml
+# else
+#     echo "Wrong env passed: can be either dev or prod"
+# fi
