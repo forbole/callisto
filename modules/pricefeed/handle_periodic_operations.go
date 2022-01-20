c746bc8d5fe0a86ed248f1707ae66c3ca10e9512
Magic Cat <pusz.monika@gmail.com>
2022-01-20 14:20:50 +0000

    Add GetTokensPriceID, remove coin list fetching

--- modules/pricefeed/handle_periodic_operations.go
+++ modules/pricefeed/handle_periodic_operations.go
@@ -2,7 +2,6 @@ package pricefeed
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/go-co-op/gocron"
 	"github.com/rs/zerolog/log"
@@ -32,32 +31,10 @@ func (m *Module) updatePrice() error {
 		Str("operation", "pricefeed").
 		Msg("getting token price and market cap")
 
-	// Get the list of coins
-	coins, err := coingecko.GetCoinsList()
-	if err != nil {
-		return fmt.Errorf("error while getting coins list: %s", err)
-	}
-
-	// Get the list of token units
-	units, err := m.db.GetTokenUnits()
+	// Get the list of token units stored in db
+	ids, err := m.db.GetTokensPriceID()
 	if err != nil {
 		return fmt.Errorf("error while getting token units: %s", err)
-	}
-
-	// Find the id of the coins
-	var ids []string
-	for _, tradedToken := range units {
-		// Skip the token if the price id is empty
-		if tradedToken.PriceID == "" {
-			continue
-		}
-
-		for _, coin := range coins {
-			if strings.EqualFold(coin.ID, tradedToken.PriceID) {
-				ids = append(ids, coin.ID)
-				break
-			}
-		}
 	}
 
 	if len(ids) == 0 {
@@ -76,6 +53,8 @@ func (m *Module) updatePrice() error {
 	if err != nil {
 		return fmt.Errorf("error while saving token prices: %s", err)
 	}
+
+	log.Debug().Str("module", "pricefeed").Msg("SAVED PRICES!!....... ELO .........")
 
 	return m.historyModule.UpdatePricesHistory(prices)
 }
