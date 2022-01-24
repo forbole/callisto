b870fdf64687d64128ba71a49ea633c56a58654e
huichiaotsou <huichiao.tsou@gmail.com>
2022-01-24 18:52:25 +0800

    change way to get balance at certain height state

--- modules/bank/source/remote/source_actions.go
+++ modules/bank/source/remote/source_actions.go
@@ -1,30 +1,43 @@
 package remote
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 
+	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	"github.com/forbole/juno/v2/node/remote"
 
 	"github.com/forbole/bdjuno/v2/types"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 // GetBalances implements bankkeeper.Source
 func (s Source) GetBalances(addresses []string, height int64) ([]types.AccountBalance, error) {
-	header := remote.GetHeightRequestHeader(height)
+	// header := remote.GetHeightRequestHeader(height)
 
+	var header metadata.MD
 	var balances []types.AccountBalance
 	for _, address := range addresses {
-		balRes, err := s.bankClient.AllBalances(s.Ctx, &banktypes.QueryAllBalancesRequest{Address: address}, header)
+		balRes, err := s.bankClient.AllBalances(
+			metadata.AppendToOutgoingContext(context.Background(), grpctypes.GRPCBlockHeightHeader, strconv.Itoa(int(height))),
+			&banktypes.QueryAllBalancesRequest{Address: address},
+			grpc.Header(&header),
+		)
 		if err != nil {
 			return nil, fmt.Errorf("error while getting all balances: %s", err)
 		}
+
+		blockHeight := header.Get(grpctypes.GRPCBlockHeightHeader)
+		fmt.Println("-- Blockheight: ", blockHeight)
 
 		balances = append(balances, types.NewAccountBalance(
 			address,
 			balRes.Balances,
 			height,
 		))
+
 	}
 
 	return balances, nil
