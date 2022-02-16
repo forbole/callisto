71ff83f5a9241c3bcc3ca3e5c485a7677e377d51
huichiaotsou <huichiao.tsou@gmail.com>
2022-02-16 10:34:33 +0800

    change decription for insecure flag

--- cmd/actions/types/flags.go
+++ cmd/actions/types/flags.go
@@ -15,5 +15,5 @@ func AddNodeFlagsToCmd(cmd *cobra.Command) {
 	cmd.PersistentFlags().StringVar(&FlagRPC, "rpc", "http://127.0.0.1:26657", "RPC listen address. Port required")
 	cmd.PersistentFlags().StringVar(&FlagGRPC, "grpc", "http://127.0.0.1:9090", "GRPC listen address. Port required")
 
-	cmd.PersistentFlags().BoolVar(&FlagInsecure, "insecure", false, "insecure or secure connection")
+	cmd.PersistentFlags().BoolVar(&FlagInsecure, "insecure", false, "Allow insecure connections")
 }
