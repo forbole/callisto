3df0f4fd80691f94dfd97dbbfcf76fff4fec3272
Aaron <76254323+huichiaotsou@users.noreply.github.com>
2022-02-16 09:50:22 +0800

    Update cmd/actions/types/flags.go
    Co-authored-by: Magic Cat <37407870+MonikaCat@users.noreply.github.com>

--- cmd/actions/types/flags.go
+++ cmd/actions/types/flags.go
@@ -12,7 +12,7 @@ var (
 
 // AddNodeFlagsToCmd adds node flags to hasura actions.
 func AddNodeFlagsToCmd(cmd *cobra.Command) {
-	cmd.PersistentFlags().StringVar(&FlagRPC, "rpc", "https://localhost:26657", "<host>:<port> to the RPC interface for the chain")
+	cmd.PersistentFlags().StringVar(&FlagRPC, "rpc", "http://127.0.0.1:26657", "RPC listen address. Port required")
 	cmd.PersistentFlags().StringVar(&FlagGRPC, "grpc", "https://localhost:9090", "<host>:<port> to the gRPC interface for the chain")
 	cmd.PersistentFlags().BoolVar(&FlagInsecure, "insecure", false, "insecure or secure connection")
 }
