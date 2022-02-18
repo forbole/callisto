5809de0bc827dff4103b0cf654beae8cd25a7c9b
Aaron <76254323+huichiaotsou@users.noreply.github.com>
2022-02-18 22:40:23 +0800

    fix: turn hasura actions coin amount type to string (#332)
    ## Description
    
    Closes: #325
    
    
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [ ] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] targeted the correct branch
    - [ ] provided a link to the relevant issue or specification
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [x] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [x] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [x] reviewed tests and test coverage
    - [x] manually tested (if applicable)

--- cmd/actions/handlers/validator_delegation.go
+++ cmd/actions/handlers/validator_delegation.go
@@ -6,8 +6,10 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	actionstypes "github.com/forbole/bdjuno/v2/cmd/actions/types"
+	dbtypes "github.com/forbole/bdjuno/v2/database/types"
 )
 
 func ValidatorDelegation(w http.ResponseWriter, r *http.Request) {
@@ -66,7 +68,7 @@ func getValidatorDelegations(input actionstypes.PayloadArgs) (response actionsty
 		delegations[index] = actionstypes.Delegation{
 			DelegatorAddress: del.Delegation.DelegatorAddress,
 			ValidatorAddress: del.Delegation.ValidatorAddress,
-			Coins:            del.Balance,
+			Coins:            dbtypes.NewDbCoins([]sdk.Coin{del.Balance}),
 		}
 	}
 
