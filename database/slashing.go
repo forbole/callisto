256a6ff8a63de28732594ef362302b4ff5297797
Magic Cat <pusz.monika@gmail.com>
2022-03-15 17:42:34 +0000

    Updated validators signing infos

--- database/slashing.go
+++ database/slashing.go
@@ -18,17 +18,19 @@ INSERT INTO validator_signing_info
     (validator_address, start_height, index_offset, jailed_until, tombstoned, missed_blocks_counter, height)
 VALUES `
 	var args []interface{}
+	counter := 0
+	for _, info := range infos {
+		if info.ValidatorAddress != "" {
+			ii := counter * 7
 
-	for i, info := range infos {
-		ii := i * 7
-
-		stmt += fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d),", ii+1, ii+2, ii+3, ii+4, ii+5, ii+6, ii+7)
-		args = append(args,
-			info.ValidatorAddress, info.StartHeight, info.IndexOffset, info.JailedUntil, info.Tombstoned,
-			info.MissedBlocksCounter, info.Height,
-		)
+			stmt += fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d),", ii+1, ii+2, ii+3, ii+4, ii+5, ii+6, ii+7)
+			args = append(args,
+				info.ValidatorAddress, info.StartHeight, info.IndexOffset, info.JailedUntil, info.Tombstoned,
+				info.MissedBlocksCounter, info.Height,
+			)
+			counter++
+		}
 	}
-
 	stmt = stmt[:len(stmt)-1] // Remove trailing ","
 	stmt += `
 ON CONFLICT (validator_address) DO UPDATE 
