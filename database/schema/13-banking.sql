5b716c80214884ae0d7bc06d71db7f8f88f65ac8
a.tereshenko <a.tereshenko@vipcoingold.com>
2022-03-26 16:37:53 +0200

    VC-4242: added tables for banking module

---
+++ database/schema/13-banking.sql
@@ -0,0 +1,74 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_banking (
+    id                  SERIAL PRIMARY KEY NOT NULL,            -- banking id
+    asset               TEXT NOT NULL,                          -- banking asset
+    amount              BIGSERIAL,                              -- banking amount
+    kind                INT,                                    -- banking amount
+    extras              JSONB,                                  -- banking extras
+    timestamp           TIMESTAMP WITHOUT TIME ZONE NOT NULL,   -- banking timestamp
+    tx_hash             TEXT NOT NULL                           -- banking tx hash
+);
+
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_system_transfer (
+    creator             TEXT NOT NULL,                      -- banking creator
+    wallet_from         TEXT NOT NULL,                      -- banking wallet from
+    wallet_to           TEXT NOT NULL,                      -- banking wallet to
+    asset               TEXT NOT NULL,                      -- banking asset
+    amount              BIGSERIAL,                          -- banking amount
+    extras              JSONB                               -- banking extras
+);
+
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_set_reward_manager_address (
+    creator            TEXT NOT NULL,                       -- banking creator
+    address            TEXT NOT NULL                        -- banking address
+);
+
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_system_reward_transfer (
+    creator             TEXT NOT NULL,                      -- banking creator
+    wallet_from         TEXT NOT NULL,                      -- banking wallet from
+    wallet_to           TEXT NOT NULL,                      -- banking wallet to
+    asset               TEXT NOT NULL,                      -- banking asset
+    amount              BIGSERIAL,                          -- banking amount
+    extras              JSONB                               -- banking extras
+);
+
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_payment (
+    creator             TEXT NOT NULL,                      -- banking creator
+    wallet_from         TEXT NOT NULL,                      -- banking wallet from
+    wallet_to           TEXT NOT NULL,                      -- banking wallet to
+    asset               TEXT NOT NULL,                      -- banking asset
+    amount              BIGSERIAL,                          -- banking amount
+    extras              JSONB                               -- banking extras
+);
+
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_withdraw (
+    creator             TEXT NOT NULL,                      -- banking creator
+    wallet              TEXT NOT NULL,                      -- banking wallet
+    asset               TEXT NOT NULL,                      -- banking asset
+    amount              BIGSERIAL,                          -- banking amount
+    extras              JSONB                               -- banking extras
+);
+
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_set_transfer_extra (
+    creator             TEXT NOT NULL,                      -- banking creator
+    id                  BIGSERIAL,                          -- banking id
+    extras              JSONB                               -- banking extras
+);
+
+CREATE TABLE IF NOT EXISTS vipcoin_chain_banking_issue (
+    creator             TEXT NOT NULL,                      -- banking creator
+    wallet              TEXT NOT NULL,                      -- banking wallet
+    asset               TEXT NOT NULL,                      -- banking asset
+    amount              BIGSERIAL,                          -- banking amount
+    extras              JSONB                               -- banking extras
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS vipcoin_chain_banking_banking CASCADE;
+DROP TABLE IF EXISTS vipcoin_chain_banking_system_transfer CASCADE;
+DROP TABLE IF EXISTS vipcoin_chain_banking_set_reward_manager_address CASCADE;
+DROP TABLE IF EXISTS vipcoin_chain_banking_system_reward_transfer CASCADE;
+DROP TABLE IF EXISTS vipcoin_chain_banking_payment CASCADE;
+DROP TABLE IF EXISTS vipcoin_chain_banking_withdraw CASCADE;
+DROP TABLE IF EXISTS vipcoin_chain_banking_set_transfer_extra CASCADE;
+DROP TABLE IF EXISTS vipcoin_chain_banking_issue CASCADE;
