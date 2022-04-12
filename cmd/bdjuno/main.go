d663508089a76c5d91753cea1bed5a206dd3c8ff
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-04-12 05:48:14 +0200

    feat: move Hasura actions to be a module (#381)
    ## Description
    This PR makes the Hasura actions work just like any other module. The configuration of the module allows to specify another node to get the data from. It also changes how the `migrate v3` command works by adding the configuration for the new `actions` module.
    
    Closes: #375
    
    
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [x] targeted the correct branch
    - [ ] provided a link to the relevant issue or specification
    - [x] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- cmd/bdjuno/main.go
+++ cmd/bdjuno/main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	parsecmd "github.com/forbole/bdjuno/v2/cmd/parse"
 	"github.com/forbole/juno/v3/cmd"
 	initcmd "github.com/forbole/juno/v3/cmd/init"
 	parsetypes "github.com/forbole/juno/v3/cmd/parse/types"
-
+	startcmd "github.com/forbole/juno/v3/cmd/start"
 	"github.com/forbole/juno/v3/modules/messages"
 
-	actionscmd "github.com/forbole/bdjuno/v2/cmd/actions"
-	migratecmd "github.com/forbole/juno/v3/cmd/migrate"
-	startcmd "github.com/forbole/juno/v3/cmd/start"
+	migratecmd "github.com/forbole/bdjuno/v2/cmd/migrate"
+	parsecmd "github.com/forbole/bdjuno/v2/cmd/parse"
 
 	"github.com/forbole/bdjuno/v2/types/config"
 
@@ -21,12 +19,16 @@ import (
 )
 
 func main() {
+	initCfg := initcmd.NewConfig().
+		WithConfigCreator(config.Creator)
+
 	parseCfg := parsetypes.NewConfig().
 		WithDBBuilder(database.Builder).
 		WithEncodingConfigBuilder(config.MakeEncodingConfig(getBasicManagers())).
 		WithRegistrar(modules.NewRegistrar(getAddressesParser()))
 
 	cfg := cmd.NewConfig("bdjuno").
+		WithInitConfig(initCfg).
 		WithParseConfig(parseCfg)
 
 	// Run the command
@@ -38,7 +40,6 @@ func main() {
 		parsecmd.NewParseCmd(cfg.GetParseConfig()),
 		migratecmd.NewMigrateCmd(cfg.GetName(), cfg.GetParseConfig()),
 		startcmd.NewStartCmd(cfg.GetParseConfig()),
-		actionscmd.NewActionsCmd(cfg.GetParseConfig()),
 	)
 
 	executor := cmd.PrepareRootCmd(cfg.GetName(), rootCmd)
