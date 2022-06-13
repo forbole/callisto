8b8bc9fe4eb9e8012106bd3a3b961f1eb6781dac
Magic Cat <pusz.monika@gmail.com>
2022-06-13 14:18:35 +0100

    add liquidstakingsource to staking module

--- modules/staking/expected_modules.go
+++ modules/staking/expected_modules.go
@@ -3,9 +3,14 @@ package staking
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	liquidstakingtypes "github.com/crescent-network/crescent/x/liquidstaking/types"
 	"github.com/forbole/bdjuno/v3/types"
 )
 
 type SlashingModule interface {
 	GetSigningInfo(height int64, consAddr sdk.ConsAddress) (types.ValidatorSigningInfo, error)
+}
+
+type LiquidStakingModule interface {
+	GetLiquidValidators(height int64) ([]liquidstakingtypes.LiquidValidatorState, error)
 }
