e9e58dddb1cfc73eacbcc9b77986c90255a88f83
Magic Cat <pusz.monika@gmail.com>
2022-06-16 17:21:35 +0100

    added schema

--- database/schema/12-marker.sql
+++ database/schema/12-marker.sql
@@ -5,3 +5,26 @@ CREATE TABLE marker_params
     height     BIGINT  NOT NULL,
     CHECK (one_row_id)
 );
+
+CREATE TABLE marker_account
+(
+    id                       SERIAL     NOT NULL PRIMARY KEY,
+    access_control           JSONB      NOT NULL DEFAULT '{}'::JSONB,
+    allow_governance_control BOOLEAN    NOT NULL,
+    base_account             JSONB      NOT NULL DEFAULT '{}'::JSONB,
+    denom                    TEXT       NOT NULL,
+    marker_type              TEXT       NOT NULL,
+    status                   TEXT       NOT NULL,
+    supply                   TEXT       NOT NULL,
+    height                   BIGINT     NOT NULL
+);
+CREATE INDEX marker_account_height_index ON marker_account (height);
+
+
+CREATE TABLE marker_acc
+(
+    id         SERIAL  NOT NULL PRIMARY KEY,
+    marker     JSONB   NOT NULL,
+    height     BIGINT  NOT NULL
+);
+CREATE INDEX marker_acc_height_index ON marker_acc (height);
