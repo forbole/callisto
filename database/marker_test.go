179f8b1618dd8017430a98807285ac316050ab42
Magic Cat <pusz.monika@gmail.com>
2022-06-17 17:54:17 +0100

    added marker params db test

---
+++ database/marker_test.go
@@ -0,0 +1,29 @@
+package database_test
+
+import (
+	"encoding/json"
+
+	"github.com/forbole/bdjuno/v3/types"
+
+	markertypes "github.com/MonikaCat/provenance/x/marker/types"
+	dbtypes "github.com/forbole/bdjuno/v3/database/types"
+)
+
+func (suite *DbTestSuite) TestBigDipperDb_SaveMarkerParams() {
+	markerParams := markertypes.NewParams(
+		1000000, true, "abc",
+	)
+	err := suite.database.SaveMarkerParams(types.NewMarkerParams(markerParams, 10))
+	suite.Require().NoError(err)
+
+	var rows []dbtypes.MarkerParamsRow
+	err = suite.database.Sqlx.Select(&rows, `SELECT * FROM marker_params`)
+	suite.Require().NoError(err)
+	suite.Require().Len(rows, 1)
+
+	var storedParams markertypes.Params
+	err = json.Unmarshal([]byte(rows[0].Params), &storedParams)
+	suite.Require().NoError(err)
+	suite.Require().Equal(markerParams, storedParams)
+	suite.Require().Equal(int64(10), rows[0].Height)
+}
