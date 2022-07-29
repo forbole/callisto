b6208b3f7bc5b43df5c4cce116194d448d87308e
Aaron <76254323+huichiaotsou@users.noreply.github.com>
2022-07-29 10:39:14 +0800

    Update modules/deployment/module.go
    Co-authored-by: Magic Cat <37407870+MonikaCat@users.noreply.github.com>

--- modules/deployment/module.go
+++ modules/deployment/module.go
@@ -11,7 +11,7 @@ var (
 	_ modules.GenesisModule = &Module{}
 )
 
-// Module represent database/deployment module
+// Module represent x/deployment module
 type Module struct {
 	cdc codec.Codec
 	db  *database.Db
