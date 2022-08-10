77996966585250766d57f14b651f3d64cf9c1059
Magic Cat <pusz.monika@gmail.com>
2022-08-10 17:15:45 +0100

    rm db func

--- database/marker.go
+++ database/marker.go
@@ -100,33 +100,3 @@ WHERE marker_account.height <= excluded.height`
 	return nil
 }
 
-// SaveMarkersTokenPrice allows to store the markers denom price for the given block height
-func (db *Db) SaveMarkersTokenPrice(prices []types.TokenPrice) error {
-	if len(prices) == 0 {
-		return nil
-	}
-
-	query := `INSERT INTO marker_token_price (unit_name, price, market_cap, timestamp) VALUES`
-	var param []interface{}
-
-	for i, ticker := range prices {
-		vi := i * 4
-		query += fmt.Sprintf("($%d,$%d,$%d,$%d),", vi+1, vi+2, vi+3, vi+4)
-		param = append(param, ticker.UnitName, ticker.Price, ticker.MarketCap, ticker.Timestamp)
-	}
-
-	query = query[:len(query)-1] // Remove trailing ","
-	query += `
-ON CONFLICT (unit_name) DO UPDATE 
-	SET price = excluded.price,
-	    market_cap = excluded.market_cap,
-	    timestamp = excluded.timestamp
-WHERE marker_token_price.timestamp <= excluded.timestamp`
-
-	_, err := db.Sql.Exec(query, param...)
-	if err != nil {
-		return fmt.Errorf("error while saving markers tokens prices: %s", err)
-	}
-
-	return nil
-}
