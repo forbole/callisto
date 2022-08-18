cab1d4368202390ccb49755f0c6b7da57accdc0d
Magic Cat <pusz.monika@gmail.com>
2022-08-18 22:53:39 +0700

    updated types

--- database/types/staking_pool.go
+++ database/types/staking_pool.go
@@ -2,21 +2,23 @@ package types
 
 // StakingPoolRow represents a single row inside the staking_pool table
 type StakingPoolRow struct {
-	OneRowID        bool  `db:"one_row_id"`
-	BondedTokens    int64 `db:"bonded_tokens"`
-	NotBondedTokens int64 `db:"not_bonded_tokens"`
-	UnbondingTokens int64 `db:"unbonding_tokens"`
-	Height          int64 `db:"height"`
+	OneRowID              bool  `db:"one_row_id"`
+	BondedTokens          int64 `db:"bonded_tokens"`
+	NotBondedTokens       int64 `db:"not_bonded_tokens"`
+	UnbondingTokens       int64 `db:"unbonding_tokens"`
+	StakedNotBondedTokens int64 `db:"staked_not_bonded_tokens"`
+	Height                int64 `db:"height"`
 }
 
 // NewStakingPoolRow allows to easily create a new StakingPoolRow
-func NewStakingPoolRow(bondedTokens, notBondedTokens, unbondingTokens int64, height int64) StakingPoolRow {
+func NewStakingPoolRow(bondedTokens, notBondedTokens, unbondingTokens, stakedNotBondedTokens int64, height int64) StakingPoolRow {
 	return StakingPoolRow{
-		OneRowID:        true,
-		BondedTokens:    bondedTokens,
-		NotBondedTokens: notBondedTokens,
-		UnbondingTokens: unbondingTokens,
-		Height:          height,
+		OneRowID:              true,
+		BondedTokens:          bondedTokens,
+		NotBondedTokens:       notBondedTokens,
+		UnbondingTokens:       unbondingTokens,
+		StakedNotBondedTokens: stakedNotBondedTokens,
+		Height:                height,
 	}
 }
 
@@ -25,5 +27,6 @@ func (r StakingPoolRow) Equal(s StakingPoolRow) bool {
 	return r.BondedTokens == s.BondedTokens &&
 		r.NotBondedTokens == s.NotBondedTokens &&
 		r.UnbondingTokens == s.UnbondingTokens &&
+		r.StakedNotBondedTokens == s.StakedNotBondedTokens &&
 		r.Height == s.Height
 }
