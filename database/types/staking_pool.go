2641c162907e058cc6e601f8d7ec3ee8e185de7d
Magic Cat <pusz.monika@gmail.com>
2022-08-18 22:08:22 +0700

    Add UnbondingTokens to Pool inst

--- database/types/staking_pool.go
+++ database/types/staking_pool.go
@@ -5,15 +5,17 @@ type StakingPoolRow struct {
 	OneRowID        bool  `db:"one_row_id"`
 	BondedTokens    int64 `db:"bonded_tokens"`
 	NotBondedTokens int64 `db:"not_bonded_tokens"`
+	UnbondingTokens int64 `db:"unbonding_tokens"`
 	Height          int64 `db:"height"`
 }
 
 // NewStakingPoolRow allows to easily create a new StakingPoolRow
-func NewStakingPoolRow(bondedTokens, notBondedTokens int64, height int64) StakingPoolRow {
+func NewStakingPoolRow(bondedTokens, notBondedTokens, unbondingTokens int64, height int64) StakingPoolRow {
 	return StakingPoolRow{
 		OneRowID:        true,
 		BondedTokens:    bondedTokens,
 		NotBondedTokens: notBondedTokens,
+		UnbondingTokens: unbondingTokens,
 		Height:          height,
 	}
 }
@@ -22,5 +24,6 @@ func NewStakingPoolRow(bondedTokens, notBondedTokens int64, height int64) Stakin
 func (r StakingPoolRow) Equal(s StakingPoolRow) bool {
 	return r.BondedTokens == s.BondedTokens &&
 		r.NotBondedTokens == s.NotBondedTokens &&
+		r.UnbondingTokens == s.UnbondingTokens &&
 		r.Height == s.Height
 }
--- types/staking_pool_params.go
+++ types/staking_pool_params.go
@@ -9,14 +9,16 @@ import (
 type Pool struct {
 	BondedTokens    sdk.Int
 	NotBondedTokens sdk.Int
+	UnbondingTokens sdk.Int
 	Height          int64
 }
 
 // NewPool allows to build a new Pool instance
-func NewPool(bondedTokens, notBondedTokens sdk.Int, height int64) *Pool {
+func NewPool(bondedTokens, notBondedTokens, unbondingTokens sdk.Int, height int64) *Pool {
 	return &Pool{
 		BondedTokens:    bondedTokens,
 		NotBondedTokens: notBondedTokens,
+		UnbondingTokens: unbondingTokens,
 		Height:          height,
 	}
 }
