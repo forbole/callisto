5f6eaa25ef66fa5e26dda27bf5f9aeed4cb9f67e
Magic Cat <pusz.monika@gmail.com>
2022-09-22 14:51:26 +0700

    add margin db tests

--- types/margin.go
+++ types/margin.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	margintypes "github.com/Sifchain/sifnode/x/margin/types"
-	sdk "github.com/tendermint/tendermint/abci/types"
+	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 // MarginParams represents the x/margin parameters
@@ -23,12 +23,12 @@ type MarginEvent struct {
 	TxHash  string
 	Index   int
 	MsgType string
-	Value   sdk.Event
+	Value   abci.Event
 	Height  int64
 }
 
 func NewMarginEvent(txHash string, index int,
-	msgType string, value sdk.Event,
+	msgType string, value abci.Event,
 	height int64) *MarginEvent {
 	return &MarginEvent{
 		TxHash:  txHash,
