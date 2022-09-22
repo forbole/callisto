cf4aa2759fa64f84c7ad25ffddd55cb101ac72ac
Magic Cat <pusz.monika@gmail.com>
2022-09-22 14:27:50 +0700

    rm involved_accounts from margin_events table

--- database/schema/12-margin.sql
+++ database/schema/12-margin.sql
@@ -14,9 +14,7 @@ CREATE TABLE margin_events
     index                       BIGINT NOT NULL,
     type                        TEXT   NOT NULL,
     value                       JSONB  NOT NULL,
-    involved_accounts_addresses TEXT[],
     height                      BIGINT NOT NULL REFERENCES block(height)
 );
 CREATE INDEX margin_events_type_index ON margin_events (type);
 CREATE INDEX margin_events_transaction_hash_index ON margin_events (transaction_hash);
-CREATE INDEX margin_events_involved_accounts_index ON margin_events USING GIN(involved_accounts_addresses);
