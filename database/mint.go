442a7f57b17de7bc86949c58c53e1c66cec0a093
huichiaotsou <huichiao.tsou@gmail.com>
2022-12-13 16:33:38 +0800

    resume infaltion

--- database/mint.go
+++ database/mint.go
@@ -4,8 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/forbole/bdjuno/v3/types"
 )
+
+// SaveInflation allows to store the inflation for the given block height as well as timestamp
+func (db *Db) SaveInflation(inflation sdk.Dec, height int64) error {
+	stmt := `
+INSERT INTO inflation (value, height) 
+VALUES ($1, $2) 
+ON CONFLICT (one_row_id) DO UPDATE 
+    SET value = excluded.value, 
+        height = excluded.height 
+WHERE inflation.height <= excluded.height`
+
+	_, err := db.Sql.Exec(stmt, inflation.String(), height)
+	if err != nil {
+		return fmt.Errorf("error while storing inflation: %s", err)
+	}
+
+	return nil
+}
 
 // SaveMintParams allows to store the given params inside the database
 func (db *Db) SaveMintParams(params *types.MintParams) error {
