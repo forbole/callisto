41b859a1a5f6fcd5ea834ffb582b6a3d87bd591e
huichiaotsou <huichiao.tsou@gmail.com>
2022-12-19 23:46:11 +0800

    to implement parse Cmd & add hasura metadata & address foreign key & test

--- database/schema/13-top_accounts.sql
+++ database/schema/13-top_accounts.sql
@@ -7,6 +7,6 @@ CREATE TABLE top_accounts
     redelegation    BIGINT DEFAULT 0,
     unbonding       BIGINT DEFAULT 0,
     reward          BIGINT DEFAULT 0,
-    sum             BIGINT  NOT NULL DEFAULT 0
+    sum             BIGINT NOT NULL DEFAULT 0
 );
 CREATE INDEX top_accounts_sum_index ON top_accounts (sum);
