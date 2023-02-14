87146f472ce6e5cf549c3391454db6e76e95a169
Magic Cat <pusz.monika@gmail.com>
2023-02-14 15:31:39 +0700

    updated staking pool snapshot handling

--- modules/gov/handle_block.go
+++ modules/gov/handle_block.go
@@ -36,9 +36,14 @@ func (m *Module) updateProposals(height int64, blockTime time.Time, blockVals *t
 			return fmt.Errorf("error while updating proposal: %s", err)
 		}
 
-		err = m.UpdateProposalSnapshots(height, blockVals, id)
+		err = m.UpdateProposalValidatorStatusesSnapshot(height, blockVals, id)
 		if err != nil {
-			return fmt.Errorf("error while updating proposal snapshots: %s", err)
+			return fmt.Errorf("error while updating proposal validator statuses snapshots: %s", err)
+		}
+
+		err = m.UpdateProposalStakingPoolSnapshot(height, blockVals, id)
+		if err != nil {
+			return fmt.Errorf("error while updating proposal validator statuses snapshots: %s", err)
 		}
 	}
 	return nil
