bc9cc2cc29cccde9a212a307f702cf142836d3ef
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2023-02-28 20:41:45 +0700

    feat: [persistence] add parse contracts cmd to wasm module  (#532)
    ## Description
    
    Closes: #XXXX
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [x] added `!` to the type prefix if API or client breaking change
    - [x] targeted the correct branch
    - [x] provided a link to the relevant issue or specification
    - [x] added a changelog entry to `CHANGELOG.md`
    - [x] included comments for [documenting Go
    code](https://blog.golang.org/godoc)
    - [x] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    I have...
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- modules/wasm/source/remote/source.go
+++ modules/wasm/source/remote/source.go
@@ -2,6 +2,7 @@ package remote
 
 import (
 	"fmt"
+	"strings"
 
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/cosmos/cosmos-sdk/types/query"
@@ -70,4 +71,52 @@ func (s Source) GetContractStates(height int64, contractAddr string) ([]wasmtype
 	}
 
 	return models, nil
+}
+
+// GetCodes implements wasmsource.Source
+func (s Source) GetCodes(height int64) ([]wasmtypes.CodeInfoResponse, error) {
+
+	var codes []wasmtypes.CodeInfoResponse
+	var nextKey []byte
+	var stop = false
+	for !stop {
+		res, err := s.wasmClient.Codes(
+			remote.GetHeightRequestContext(s.Ctx, height),
+			&wasmtypes.QueryCodesRequest{
+				Pagination: &query.PageRequest{
+					Key:   nextKey,
+					Limit: 100, // Query 100 states at time
+				},
+			},
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error while getting contract codes: %s", err)
+		}
+
+		nextKey = res.Pagination.NextKey
+		stop = len(res.Pagination.NextKey) == 0
+		codes = append(codes, res.CodeInfos...)
+	}
+
+	return codes, nil
+}
+
+// GetContractsByCode implements wasmsource.Source
+func (s Source) GetContractsByCode(height int64, codeID uint64) ([]string, error) {
+	var contracts []string
+	res, err := s.wasmClient.ContractsByCode(
+		remote.GetHeightRequestContext(s.Ctx, height),
+		&wasmtypes.QueryContractsByCodeRequest{
+			CodeId: codeID,
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error while getting contracts by code info: %s", err)
+	}
+
+	for _, c := range res.Contracts {
+		v := strings.Split(c, ",") // Split the values
+		contracts = append(contracts, v...)
+	}
+	return contracts, nil
 }
