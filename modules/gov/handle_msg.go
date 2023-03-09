0d821f38d909cb6612a4083a57e3d5d2bd4b488e
Magic Cat <pusz.monika@gmail.com>
2023-03-09 13:56:34 +0700

    rm handleConsumerRemovalProposal & handleConsumerAdditionProposal from msg handler

--- modules/gov/handle_msg.go
+++ modules/gov/handle_msg.go
@@ -2,7 +2,6 @@ package gov
 
 import (
 	"fmt"
-	"time"
 
 	"strconv"
 
@@ -10,8 +9,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	ibcclienttypes "github.com/cosmos/ibc-go/v4/modules/core/02-client/types"
-	ccvprovidertypes "github.com/cosmos/interchain-security/x/ccv/provider/types"
 	juno "github.com/forbole/juno/v4/types"
 )
 
@@ -23,17 +20,9 @@ func (m *Module) HandleMsg(index int, msg sdk.Msg, tx *juno.Tx) error {
 
 	switch cosmosMsg := msg.(type) {
 	case *govtypes.MsgSubmitProposal:
-		switch c := cosmosMsg.GetContent().(type) {
-		case *ccvprovidertypes.ConsumerAdditionProposal:
-			return m.handleConsumerAdditionProposal(c, tx, 0, cosmosMsg)
-		case *ccvprovidertypes.ConsumerRemovalProposal:
-			return m.handleConsumerRemovalProposal(c, tx, 0, cosmosMsg)
-		default:
-			return m.handleMsgSubmitProposal(tx, index, cosmosMsg)
-		}
+		return m.handleMsgSubmitProposal(tx, index, cosmosMsg)
 	case *govtypes.MsgDeposit:
 		return m.handleMsgDeposit(tx, cosmosMsg)
-
 	case *govtypes.MsgVote:
 		return m.handleMsgVote(tx, cosmosMsg)
 	}
@@ -77,7 +66,7 @@ func (m *Module) handleMsgSubmitProposal(tx *juno.Tx, index int, msg *govtypes.M
 		proposal.ProposalId,
 		proposal.ProposalRoute(),
 		proposal.ProposalType(),
-		proposal.GetContent(),
+		msg.GetContent(),
 		proposal.Status.String(),
 		proposal.SubmitTime,
 		proposal.DepositEndTime,
@@ -111,109 +100,4 @@ func (m *Module) handleMsgDeposit(tx *juno.Tx, msg *govtypes.MsgDeposit) error {
 func (m *Module) handleMsgVote(tx *juno.Tx, msg *govtypes.MsgVote) error {
 	vote := types.NewVote(msg.ProposalId, msg.Voter, msg.Option, tx.Height)
 	return m.db.SaveVote(vote)
-}
-
-// handleConsumerAdditionProposal allows to properly handle a ConsumerAdditionProposal
-func (m *Module) handleConsumerAdditionProposal(consAddProposal *ccvprovidertypes.ConsumerAdditionProposal, tx *juno.Tx, index int, msg *govtypes.MsgSubmitProposal) error {
-	// Get the proposal id
-	event, err := tx.FindEventByType(index, govtypes.EventTypeSubmitProposal)
-	if err != nil {
-		return fmt.Errorf("error while searching for EventTypeSubmitProposal: %s", err)
-	}
-
-	id, err := tx.FindAttributeByKey(event, govtypes.AttributeKeyProposalID)
-	if err != nil {
-		return fmt.Errorf("error while searching for AttributeKeyProposalID: %s", err)
-	}
-
-	proposalID, err := strconv.ParseUint(id, 10, 64)
-	if err != nil {
-		return fmt.Errorf("error while parsing proposal id: %s", err)
-	}
-
-	// Get the proposal
-	proposal, err := m.source.Proposal(tx.Height, proposalID)
-	if err != nil {
-		return fmt.Errorf("error while getting proposal: %s", err)
-	}
-
-	// Store the ccv ConsumerAdditionProposal proposal
-	ccvProposalContent := types.NewCcvProposalContent(consAddProposal.Title, consAddProposal.Description, consAddProposal.ChainId, string(consAddProposal.GenesisHash), string(consAddProposal.BinaryHash),
-		consAddProposal.SpawnTime, time.Time{}, consAddProposal.InitialHeight, consAddProposal.UnbondingPeriod, consAddProposal.CcvTimeoutPeriod,
-		consAddProposal.TransferTimeoutPeriod, consAddProposal.ConsumerRedistributionFraction, consAddProposal.BlocksPerDistributionTransmission,
-		consAddProposal.HistoricalEntries)
-
-	ccvProposalObj := types.NewCcvProposal(
-		proposal.ProposalId,
-		consAddProposal.ProposalRoute(),
-		consAddProposal.ProposalType(),
-		ccvProposalContent,
-		proposal.Status.String(),
-		proposal.SubmitTime,
-		proposal.DepositEndTime,
-		proposal.VotingStartTime,
-		proposal.VotingEndTime,
-		msg.Proposer,
-	)
-
-	err = m.db.SaveCcvProposals(tx.Height, []types.CcvProposal{ccvProposalObj})
-	if err != nil {
-		return err
-	}
-
-	// Store the deposit
-	deposit := types.NewDeposit(proposal.ProposalId, msg.Proposer, msg.InitialDeposit, tx.Height)
-	return m.db.SaveDeposits([]types.Deposit{deposit})
-}
-
-// handleConsumerRemovalProposal allows to properly handle a ConsumerRemovalProposal
-func (m *Module) handleConsumerRemovalProposal(consRemProposal *ccvprovidertypes.ConsumerRemovalProposal, tx *juno.Tx, index int, msg *govtypes.MsgSubmitProposal) error {
-	// Get the proposal id
-	event, err := tx.FindEventByType(index, govtypes.EventTypeSubmitProposal)
-	if err != nil {
-		return fmt.Errorf("error while searching for EventTypeSubmitProposal: %s", err)
-	}
-
-	id, err := tx.FindAttributeByKey(event, govtypes.AttributeKeyProposalID)
-	if err != nil {
-		return fmt.Errorf("error while searching for AttributeKeyProposalID: %s", err)
-	}
-
-	proposalID, err := strconv.ParseUint(id, 10, 64)
-	if err != nil {
-		return fmt.Errorf("error while parsing proposal id: %s", err)
-	}
-
-	// Get the proposal
-	proposal, err := m.source.Proposal(tx.Height, proposalID)
-	if err != nil {
-		return fmt.Errorf("error while getting proposal: %s", err)
-	}
-
-	// Store the ccv ConsumerRemovalProposal proposal
-	ccvProposalContent := types.NewCcvProposalContent(consRemProposal.Title, consRemProposal.Description,
-		consRemProposal.ChainId, "", "", time.Time{}, consRemProposal.StopTime, ibcclienttypes.ZeroHeight(),
-		0, 0, 0, "", 0, 0)
-
-	ccvProposalObj := types.NewCcvProposal(
-		proposal.ProposalId,
-		consRemProposal.ProposalRoute(),
-		consRemProposal.ProposalType(),
-		ccvProposalContent,
-		proposal.Status.String(),
-		proposal.SubmitTime,
-		proposal.DepositEndTime,
-		proposal.VotingStartTime,
-		proposal.VotingEndTime,
-		msg.Proposer,
-	)
-
-	err = m.db.SaveCcvProposals(tx.Height, []types.CcvProposal{ccvProposalObj})
-	if err != nil {
-		return err
-	}
-
-	// Store the deposit
-	deposit := types.NewDeposit(proposal.ProposalId, msg.Proposer, msg.InitialDeposit, tx.Height)
-	return m.db.SaveDeposits([]types.Deposit{deposit})
 }
