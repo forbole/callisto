3c5c7102a4c44139246a221960539ba9137badc5
Magic Cat <pusz.monika@gmail.com>
2023-04-24 19:07:38 +0700

    add top account type

--- types/auth.go
+++ types/auth.go
@@ -11,3 +11,17 @@ func NewAccount(address string) Account {
 		Address: address,
 	}
 }
+
+// TopAccount represents a cheqd top account
+type TopAccount struct {
+	Address     string
+	AccountType string
+}
+
+// TopAccount builds a new TopAccount instance
+func NewTopAccount(address, accountType string) TopAccount {
+	return TopAccount{
+		Address:     address,
+		AccountType: accountType,
+	}
+}
