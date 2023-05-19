8c52b08e9f6382f3dd70d13a550e7e9ad965ba81
Magic Cat <pusz.monika@gmail.com>
2023-05-19 15:49:56 +0700

    updated registar.go

--- modules/registrar.go
+++ modules/registrar.go
@@ -1,7 +1,6 @@
 package modules
 
 import (
-	"github.com/forbole/bdjuno/v4/modules/actions"
 	"github.com/forbole/bdjuno/v4/modules/types"
 
 	"github.com/forbole/juno/v4/modules/pruning"
@@ -22,11 +21,9 @@ import (
 	ccvconsumer "github.com/forbole/bdjuno/v4/modules/ccv/consumer"
 	"github.com/forbole/bdjuno/v4/modules/consensus"
 	dailyrefetch "github.com/forbole/bdjuno/v4/modules/daily_refetch"
-	"github.com/forbole/bdjuno/v4/modules/distribution"
 	"github.com/forbole/bdjuno/v4/modules/feegrant"
 	"github.com/forbole/bdjuno/v4/modules/modules"
 	"github.com/forbole/bdjuno/v4/modules/pricefeed"
-	"github.com/forbole/bdjuno/v4/modules/upgrade"
 	"github.com/forbole/bdjuno/v4/modules/wasm"
 )
 
@@ -70,36 +67,28 @@ func (r *Registrar) BuildModules(ctx registrar.Context) jmodules.Modules {
 		panic(err)
 	}
 
-	actionsModule := actions.NewModule(ctx.JunoConfig, ctx.EncodingConfig)
 	authModule := auth.NewModule(r.parser, cdc, db)
 	bankModule := bank.NewModule(r.parser, sources.BankSource, cdc, db)
 	consensusModule := consensus.NewModule(db)
 	ccvConsumerModule := ccvconsumer.NewModule(sources.CcvConsumerSource, cdc, db)
 	dailyRefetchModule := dailyrefetch.NewModule(ctx.Proxy, db)
-	distrModule := distribution.NewModule(sources.DistrSource, cdc, db)
 	feegrantModule := feegrant.NewModule(cdc, db)
 	slashingModule := slashing.NewModule(sources.SlashingSource, cdc, db)
-	upgradeModule := upgrade.NewModule(db, stakingModule)
-	// upgradeModule := upgrade.NewModule(db, stakingModule)
 	wasmModule := wasm.NewModule(sources.WasmSource, cdc, db)
 
 	return []jmodules.Module{
 		messages.NewModule(r.parser, cdc, ctx.Database),
 		telemetry.NewModule(ctx.JunoConfig),
 		pruning.NewModule(ctx.JunoConfig, db, ctx.Logger),
-
-		actionsModule,
 		authModule,
 		bankModule,
 		consensusModule,
 		ccvConsumerModule,
 		dailyRefetchModule,
-		distrModule,
 		feegrantModule,
 		modules.NewModule(ctx.JunoConfig.Chain, db),
 		pricefeed.NewModule(ctx.JunoConfig, cdc, db),
 		slashingModule,
-		upgradeModule,
 		wasmModule,
 	}
 }
