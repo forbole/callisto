d75cd2cf0b0a167f0207072028f6176ce82823ab
Magic Cat <pusz.monika@gmail.com>
2023-07-11 13:07:13 +0700

    disable local mode

--- modules/bank/source/local/source.go
+++ modules/bank/source/local/source.go
@@ -1,98 +1,98 @@
 package local
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/types/query"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+// 	"github.com/cosmos/cosmos-sdk/types/query"
+// 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/forbole/juno/v4/node/local"
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	"github.com/forbole/juno/v4/node/local"
 
-	"github.com/forbole/bdjuno/v4/modules/bank/source"
-	"github.com/forbole/bdjuno/v4/types"
-)
+// 	"github.com/forbole/bdjuno/v4/modules/bank/source"
+// 	"github.com/forbole/bdjuno/v4/types"
+// )
 
-var (
-	_ source.Source = &Source{}
-)
+// var (
+// 	_ source.Source = &Source{}
+// )
 
-// Source represents the implementation of the bank keeper that works on a local node
-type Source struct {
-	*local.Source
-	q banktypes.QueryServer
-}
+// // Source represents the implementation of the bank keeper that works on a local node
+// type Source struct {
+// 	*local.Source
+// 	q banktypes.QueryServer
+// }
 
-// NewSource builds a new Source instance
-func NewSource(source *local.Source, bk banktypes.QueryServer) *Source {
-	return &Source{
-		Source: source,
-		q:      bk,
-	}
-}
+// // NewSource builds a new Source instance
+// func NewSource(source *local.Source, bk banktypes.QueryServer) *Source {
+// 	return &Source{
+// 		Source: source,
+// 		q:      bk,
+// 	}
+// }
 
-// GetBalances implements keeper.Source
-func (s Source) GetBalances(addresses []string, height int64) ([]types.AccountBalance, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // GetBalances implements keeper.Source
+// func (s Source) GetBalances(addresses []string, height int64) ([]types.AccountBalance, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	var balances []types.AccountBalance
-	for _, address := range addresses {
-		res, err := s.q.AllBalances(sdk.WrapSDKContext(ctx), &banktypes.QueryAllBalancesRequest{Address: address})
-		if err != nil {
-			return nil, err
-		}
+// 	var balances []types.AccountBalance
+// 	for _, address := range addresses {
+// 		res, err := s.q.AllBalances(sdk.WrapSDKContext(ctx), &banktypes.QueryAllBalancesRequest{Address: address})
+// 		if err != nil {
+// 			return nil, err
+// 		}
 
-		balances = append(balances, types.NewAccountBalance(address, res.Balances, height))
-	}
+// 		balances = append(balances, types.NewAccountBalance(address, res.Balances, height))
+// 	}
 
-	return balances, nil
-}
+// 	return balances, nil
+// }
 
-// GetSupply implements bankkeeper.Source
-func (s Source) GetSupply(height int64) (sdk.Coins, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // GetSupply implements bankkeeper.Source
+// func (s Source) GetSupply(height int64) (sdk.Coins, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	var coins []sdk.Coin
-	var nextKey []byte
-	var stop = false
-	for !stop {
-		res, err := s.q.TotalSupply(
-			sdk.WrapSDKContext(ctx),
-			&banktypes.QueryTotalSupplyRequest{
-				Pagination: &query.PageRequest{
-					Key:   nextKey,
-					Limit: 100, // Query 100 supplies at time
-				},
-			})
-		if err != nil {
-			return nil, fmt.Errorf("error while getting total supply: %s", err)
-		}
+// 	var coins []sdk.Coin
+// 	var nextKey []byte
+// 	var stop = false
+// 	for !stop {
+// 		res, err := s.q.TotalSupply(
+// 			sdk.WrapSDKContext(ctx),
+// 			&banktypes.QueryTotalSupplyRequest{
+// 				Pagination: &query.PageRequest{
+// 					Key:   nextKey,
+// 					Limit: 100, // Query 100 supplies at time
+// 				},
+// 			})
+// 		if err != nil {
+// 			return nil, fmt.Errorf("error while getting total supply: %s", err)
+// 		}
 
-		nextKey = res.Pagination.NextKey
-		stop = len(res.Pagination.NextKey) == 0
-		coins = append(coins, res.Supply...)
-	}
+// 		nextKey = res.Pagination.NextKey
+// 		stop = len(res.Pagination.NextKey) == 0
+// 		coins = append(coins, res.Supply...)
+// 	}
 
-	return coins, nil
-}
+// 	return coins, nil
+// }
 
-// GetAccountBalances implements bankkeeper.Source
-func (s Source) GetAccountBalance(address string, height int64) ([]sdk.Coin, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // GetAccountBalances implements bankkeeper.Source
+// func (s Source) GetAccountBalance(address string, height int64) ([]sdk.Coin, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	balRes, err := s.q.AllBalances(sdk.WrapSDKContext(ctx), &banktypes.QueryAllBalancesRequest{Address: address})
-	if err != nil {
-		return nil, fmt.Errorf("error while getting all balances: %s", err)
-	}
+// 	balRes, err := s.q.AllBalances(sdk.WrapSDKContext(ctx), &banktypes.QueryAllBalancesRequest{Address: address})
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while getting all balances: %s", err)
+// 	}
 
-	return balRes.Balances, nil
-}
+// 	return balRes.Balances, nil
+// }
--- modules/gov/source/local/source.go
+++ modules/gov/source/local/source.go
@@ -1,123 +1,123 @@
 package local
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
-	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+// 	govtypesv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 
-	"github.com/forbole/juno/v4/node/local"
+// 	"github.com/forbole/juno/v4/node/local"
 
-	govsource "github.com/forbole/bdjuno/v4/modules/gov/source"
-)
+// 	govsource "github.com/forbole/bdjuno/v4/modules/gov/source"
+// )
 
-var (
-	_ govsource.Source = &Source{}
-)
+// var (
+// 	_ govsource.Source = &Source{}
+// )
 
-// Source implements govsource.Source by using a local node
-type Source struct {
-	*local.Source
-	q        govtypesv1.QueryServer
-	qv1beta1 govtypesv1beta1.QueryClient
-}
+// // Source implements govsource.Source by using a local node
+// type Source struct {
+// 	*local.Source
+// 	q        govtypesv1.QueryServer
+// 	qv1beta1 govtypesv1beta1.QueryClient
+// }
 
-// NewSource returns a new Source instance
-func NewSource(source *local.Source, govKeeper govtypesv1.QueryServer, govKeeperv1beta1 govtypesv1beta1.QueryClient) *Source {
-	return &Source{
-		Source:   source,
-		q:        govKeeper,
-		qv1beta1: govKeeperv1beta1,
-	}
-}
+// // NewSource returns a new Source instance
+// func NewSource(source *local.Source, govKeeper govtypesv1.QueryServer, govKeeperv1beta1 govtypesv1beta1.QueryClient) *Source {
+// 	return &Source{
+// 		Source:   source,
+// 		q:        govKeeper,
+// 		qv1beta1: govKeeperv1beta1,
+// 	}
+// }
 
-// Proposal implements govsource.Source
-func (s Source) Proposal(height int64, id uint64) (govtypesv1beta1.Proposal, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return govtypesv1beta1.Proposal{}, fmt.Errorf("error while loading height: %s", err)
-	}
+// // Proposal implements govsource.Source
+// func (s Source) Proposal(height int64, id uint64) (govtypesv1beta1.Proposal, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return govtypesv1beta1.Proposal{}, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	res, err := s.qv1beta1.Proposal(sdk.WrapSDKContext(ctx), &govtypesv1beta1.QueryProposalRequest{ProposalId: id})
-	if err != nil {
-		return govtypesv1beta1.Proposal{}, err
-	}
+// 	res, err := s.qv1beta1.Proposal(sdk.WrapSDKContext(ctx), &govtypesv1beta1.QueryProposalRequest{ProposalId: id})
+// 	if err != nil {
+// 		return govtypesv1beta1.Proposal{}, err
+// 	}
 
-	return res.Proposal, nil
-}
+// 	return res.Proposal, nil
+// }
 
-// ProposalDeposit implements govsource.Source
-func (s Source) ProposalDeposit(height int64, id uint64, depositor string) (*govtypesv1.Deposit, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // ProposalDeposit implements govsource.Source
+// func (s Source) ProposalDeposit(height int64, id uint64, depositor string) (*govtypesv1.Deposit, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	res, err := s.q.Deposit(sdk.WrapSDKContext(ctx), &govtypesv1.QueryDepositRequest{ProposalId: id, Depositor: depositor})
-	if err != nil {
-		return nil, err
-	}
+// 	res, err := s.q.Deposit(sdk.WrapSDKContext(ctx), &govtypesv1.QueryDepositRequest{ProposalId: id, Depositor: depositor})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return res.Deposit, nil
-}
+// 	return res.Deposit, nil
+// }
 
-// TallyResult implements govsource.Source
-func (s Source) TallyResult(height int64, proposalID uint64) (*govtypesv1.TallyResult, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // TallyResult implements govsource.Source
+// func (s Source) TallyResult(height int64, proposalID uint64) (*govtypesv1.TallyResult, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	res, err := s.q.TallyResult(sdk.WrapSDKContext(ctx), &govtypesv1.QueryTallyResultRequest{ProposalId: proposalID})
-	if err != nil {
-		return nil, err
-	}
+// 	res, err := s.q.TallyResult(sdk.WrapSDKContext(ctx), &govtypesv1.QueryTallyResultRequest{ProposalId: proposalID})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return res.Tally, nil
-}
+// 	return res.Tally, nil
+// }
 
-// DepositParams implements govsource.Source
-func (s Source) DepositParams(height int64) (*govtypesv1.DepositParams, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // DepositParams implements govsource.Source
+// func (s Source) DepositParams(height int64) (*govtypesv1.DepositParams, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	res, err := s.q.Params(sdk.WrapSDKContext(ctx), &govtypesv1.QueryParamsRequest{ParamsType: govtypesv1.ParamDeposit})
-	if err != nil {
-		return nil, err
-	}
+// 	res, err := s.q.Params(sdk.WrapSDKContext(ctx), &govtypesv1.QueryParamsRequest{ParamsType: govtypesv1.ParamDeposit})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return res.DepositParams, nil
-}
+// 	return res.DepositParams, nil
+// }
 
-// VotingParams implements govsource.Source
-func (s Source) VotingParams(height int64) (*govtypesv1.VotingParams, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // VotingParams implements govsource.Source
+// func (s Source) VotingParams(height int64) (*govtypesv1.VotingParams, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	res, err := s.q.Params(sdk.WrapSDKContext(ctx), &govtypesv1.QueryParamsRequest{ParamsType: govtypesv1.ParamVoting})
-	if err != nil {
-		return nil, err
-	}
+// 	res, err := s.q.Params(sdk.WrapSDKContext(ctx), &govtypesv1.QueryParamsRequest{ParamsType: govtypesv1.ParamVoting})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return res.VotingParams, nil
-}
+// 	return res.VotingParams, nil
+// }
 
-// TallyParams implements govsource.Source
-func (s Source) TallyParams(height int64) (*govtypesv1.TallyParams, error) {
-	ctx, err := s.LoadHeight(height)
-	if err != nil {
-		return nil, fmt.Errorf("error while loading height: %s", err)
-	}
+// // TallyParams implements govsource.Source
+// func (s Source) TallyParams(height int64) (*govtypesv1.TallyParams, error) {
+// 	ctx, err := s.LoadHeight(height)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error while loading height: %s", err)
+// 	}
 
-	res, err := s.q.Params(sdk.WrapSDKContext(ctx), &govtypesv1.QueryParamsRequest{ParamsType: govtypesv1.ParamTallying})
-	if err != nil {
-		return nil, err
-	}
+// 	res, err := s.q.Params(sdk.WrapSDKContext(ctx), &govtypesv1.QueryParamsRequest{ParamsType: govtypesv1.ParamTallying})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return res.TallyParams, nil
-}
+// 	return res.TallyParams, nil
+// }
