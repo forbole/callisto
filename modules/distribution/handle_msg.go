068cab1d7ba665922483f1205e90e0ef06ca266b
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-09-11 09:23:34 -0500

    feat: add MsgExec support for modules

--- modules/distribution/handle_msg.go
+++ modules/distribution/handle_msg.go
@@ -2,10 +2,16 @@ package distribution
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
 	juno "github.com/forbole/juno/v4/types"
 
 	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 )
+
+// HandleMsgExec implements modules.AuthzMessageModule
+func (m *Module) HandleMsgExec(index int, _ *authz.MsgExec, _ int, executedMsg sdk.Msg, tx *juno.Tx) error {
+	return m.HandleMsg(index, executedMsg, tx)
+}
 
 // HandleMsg implements modules.MessageModule
 func (m *Module) HandleMsg(_ int, msg sdk.Msg, tx *juno.Tx) error {
