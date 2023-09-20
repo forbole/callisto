soar-robotics/soarchain-bdjuno

updated gov block handler to update proposals included in EndBlockEvents

modules/gov/handle_block.go
@@ -2,10 +2,13 @@ package gov
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	juno "github.com/forbole/juno/v4/types"
 
+	abci "github.com/tendermint/tendermint/abci/types"
 	tmctypes "github.com/tendermint/tendermint/rpc/core/types"
 
 	"github.com/rs/zerolog/log"
@@ -13,38 +16,54 @@ import (
 
 // HandleBlock implements modules.BlockModule
 func (m *Module) HandleBlock(
-	b *tmctypes.ResultBlock, _ *tmctypes.ResultBlockResults, _ []*juno.Tx, vals *tmctypes.ResultValidators,
+	b *tmctypes.ResultBlock, blockResults *tmctypes.ResultBlockResults, txs []*juno.Tx, vals *tmctypes.ResultValidators,
 ) error {
-	err := m.updateProposals(b.Block.Height, b.Block.Time, vals)
+
+	err := m.updateProposalsStatus(b.Block.Height, b.Block.Time, vals, blockResults.EndBlockEvents)
 	if err != nil {
 		log.Error().Str("module", "gov").Int64("height", b.Block.Height).
 			Err(err).Msg("error while updating proposals")
 	}
+
 	return nil
 }
 
-// updateProposals updates the proposals
-func (m *Module) updateProposals(height int64, blockTime time.Time, blockVals *tmctypes.ResultValidators) error {
-	ids, err := m.db.GetOpenProposalsIds(blockTime)
-	if err != nil {
-		log.Error().Err(err).Str("module", "gov").Msg("error while getting open ids")
+// updateProposalsStatus updates the status of proposals if they have been included in the EndBlockEvents
+func (m *Module) updateProposalsStatus(height int64, blockTime time.Time, blockVals *tmctypes.ResultValidators, events []abci.Event) error {
+	if len(events) == 0 {
+		return nil
 	}
 
+	var ids []uint64
+	eventsList := juno.FindEventsByType(events, govtypes.EventTypeActiveProposal)
+	if len(eventsList) == 0 {
+		return nil
+	}
+
+	for _, event := range eventsList {
+		// find proposal ID
+		proposalID, err := juno.FindAttributeByKey(event, govtypes.AttributeKeyProposalID)
+		if err != nil {
+			return fmt.Errorf("error while getting proposal ID from block events: %s", err)
+		}
+
+		// parse proposal ID from []byte to unit64
+		id, err := strconv.ParseUint(string(proposalID.Value), 10, 64)
+		if err != nil {
+			return fmt.Errorf("error while parsing proposal id: %s", err)
+		}
+
+		// add proposal ID to ids array
+		ids = append(ids, id)
+	}
+
+	// update status for proposals IDs stored in ids array
 	for _, id := range ids {
-		err = m.UpdateProposal(height, blockTime, id)
+		err := m.UpdateProposalStatus(height, blockTime, id)
 		if err != nil {
-			return fmt.Errorf("error while updating proposal: %s", err)
-		}
-
-		err = m.UpdateProposalValidatorStatusesSnapshot(height, blockVals, id)
-		if err != nil {
-			return fmt.Errorf("error while updating proposal validator statuses snapshots: %s", err)
-		}
-
-		err = m.UpdateProposalStakingPoolSnapshot(height, blockVals, id)
-		if err != nil {
-			return fmt.Errorf("error while updating proposal validator statuses snapshots: %s", err)
+			return fmt.Errorf("error while updating proposal %d status: %s", id, err)
 		}
 	}
+
 	return nil
 }

forbole/callisto

modules/gov/handle_block.go
@@ -2,49 +2,70 @@ package gov
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	juno "github.com/forbole/juno/v5/types"
 
 	tmctypes "github.com/cometbft/cometbft/rpc/core/types"
 
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	abci "github.com/cometbft/cometbft/abci/types"
+
+
 	"github.com/rs/zerolog/log"
 )
 
 // HandleBlock implements modules.BlockModule
 func (m *Module) HandleBlock(
-	b *tmctypes.ResultBlock, _ *tmctypes.ResultBlockResults, _ []*juno.Tx, vals *tmctypes.ResultValidators,
+	b *tmctypes.ResultBlock, blockResults *tmctypes.ResultBlockResults, txs []*juno.Tx, vals *tmctypes.ResultValidators,
 ) error {
-	err := m.updateProposals(b.Block.Height, b.Block.Time, vals)
+
+	err := m.updateProposalsStatus(b.Block.Height, b.Block.Time, vals, blockResults.EndBlockEvents)
 	if err != nil {
 		log.Error().Str("module", "gov").Int64("height", b.Block.Height).
 			Err(err).Msg("error while updating proposals")
 	}
+
 	return nil
 }
 
-// updateProposals updates the proposals
-func (m *Module) updateProposals(height int64, blockTime time.Time, blockVals *tmctypes.ResultValidators) error {
-	ids, err := m.db.GetOpenProposalsIds(blockTime)
-	if err != nil {
-		log.Error().Err(err).Str("module", "gov").Msg("error while getting open ids")
+// updateProposalsStatus updates the status of proposals if they have been included in the EndBlockEvents
+func (m *Module) updateProposalsStatus(height int64, blockTime time.Time, blockVals *tmctypes.ResultValidators, events []abci.Event) error {
+	if len(events) == 0 {
+		return nil
 	}
 
+	var ids []uint64
+	eventsList := juno.FindEventsByType(events, govtypes.EventTypeActiveProposal)
+	if len(eventsList) == 0 {
+		return nil
+	}
+
+	for _, event := range eventsList {
+		// find proposal ID
+		proposalID, err := juno.FindAttributeByKey(event, govtypes.AttributeKeyProposalID)
+		if err != nil {
+			return fmt.Errorf("error while getting proposal ID from block events: %s", err)
+		}
+
+		// parse proposal ID from []byte to unit64
+		id, err := strconv.ParseUint(string(proposalID.Value), 10, 64)
+		if err != nil {
+			return fmt.Errorf("error while parsing proposal id: %s", err)
+		}
+
+		// add proposal ID to ids array
+		ids = append(ids, id)
+	}
+
+	// update status for proposals IDs stored in ids array
 	for _, id := range ids {
-		err = m.UpdateProposal(height, blockTime, id)
+		err := m.UpdateProposalStatus(height, blockTime, id)
 		if err != nil {
-			return fmt.Errorf("error while updating proposal: %s", err)
-		}
-
-		err = m.UpdateProposalValidatorStatusesSnapshot(height, blockVals, id)
-		if err != nil {
-			return fmt.Errorf("error while updating proposal validator statuses snapshots: %s", err)
-		}
-
-		err = m.UpdateProposalStakingPoolSnapshot(height, blockVals, id)
-		if err != nil {
-			return fmt.Errorf("error while updating proposal validator statuses snapshots: %s", err)
+			return fmt.Errorf("error while updating proposal %d status: %s", id, err)
 		}
 	}
+
 	return nil
 }