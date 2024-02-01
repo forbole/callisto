ea8ff484cbe98e25e21b3e98fb8e5569f6531930
Magic Cat <pusz.monika@gmail.com>
2024-02-01 18:44:46 +0700

    feat: store msg types inside message_type table and add messages_by_type function

---
+++ database/message_type.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	types "github.com/forbole/bdjuno/v4/types"
+)
+
+// SaveMessageType stores the given message type inside the database
+func (db *Db) SaveMessageType(msg *types.MessageType) error {
+	stmt := `
+INSERT INTO message_type(type, module, label, height) 
+VALUES ($1, $2, $3, $4) 
+ON CONFLICT (type) DO NOTHING`
+
+	_, err := db.SQL.Exec(stmt, msg.Type, msg.Module, msg.Label, msg.Height)
+	return err
+}
